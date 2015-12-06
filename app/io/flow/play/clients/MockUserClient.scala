d9d77eac6d871487f2ee16eac25e94e17623f854
Michael Bryzek <mbryzek@alum.mit.edu>
2015-12-06 12:55:23 -0500

    Add helper factor methods for testing

--- app/io/flow/play/clients/MockUserClient.scala
+++ app/io/flow/play/clients/MockUserClient.scala
@@ -1,7 +1,9 @@
 package io.flow.play.clients
 
+import io.flow.common.v0.models.{Audit, Reference}
+import io.flow.user.v0.models.{Name, NameForm, User, UserForm}
 import java.util.UUID
-import io.flow.user.v0.models.User
+import org.joda.time.DateTime
 import scala.concurrent.{ExecutionContext, Future}
 import scala.util.{Failure, Success, Try}
 
@@ -34,6 +36,43 @@ object MockUserClient {
 
   private var usersByGuid = scala.collection.mutable.Map[UUID, User]()
   private var usersByToken = scala.collection.mutable.Map[String, User]()
+
+  def makeAudit(): Audit = {
+    val reference = Reference(UUID.randomUUID())
+    val now = new DateTime()
+    Audit(
+      createdAt = now,
+      createdBy = reference,
+      updatedAt = now,
+      updatedBy = reference
+    )
+  }
+
+  /**
+    * Constructs a user object - in memory only.
+    */
+  def makeUser(
+    form: UserForm = makeUserForm()
+  ): User = {
+    User(
+      guid = UUID.randomUUID,
+      email = form.email,
+      name = form.name match {
+        case None => Name()
+        case Some(n) => Name(
+          first = n.first,
+          last = n.first
+        )
+      },
+      audit = makeAudit
+    )
+  }
+
+  def makeUserForm() = UserForm(
+    email = None,
+    name = None,
+    avatarUrl = None
+  )
 
   def add(
     user: User,

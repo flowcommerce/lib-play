3dda41f702c9378edd524b965da03bf679fd177d
Michael Bryzek <mbryzek@alum.mit.edu>
2015-12-29 09:36:13 -0500

    Provide cleaner trait for IdGenerator

--- app/io/flow/play/util/IdGenerator.scala
+++ app/io/flow/play/util/IdGenerator.scala
@@ -1,34 +1,37 @@
 package io.flow.play.util
 
+object IdGenerator {
+
+  case object Carrier extends IdGenerator { override val prefix = "car" }
+  case object Center extends IdGenerator { override val prefix = "ctr" }
+  case object Region extends IdGenerator { override val prefix = "reg" }
+  case object Ruleset extends IdGenerator { override val prefix = "rls" }
+  case object ServiceLevel extends IdGenerator { override val prefix = "svl" }
+
+  private[util] val Separator = "-"
+  private[IdGenerator] val DefaultLength = 20
+
+}
+
 /**
   * Unique set of prefixes we use when generated objects. This is here
   * to centralize the allocation of prefixes used for unique
   * identifiers to avoid conflicts.
   */
-sealed trait Prefix
-
-object Prefix {
-
-  case object Carrier extends Prefix { override def toString() = "car" }
-  case object Center extends Prefix { override def toString() = "ctr" }
-  case object Region extends Prefix { override def toString() = "reg" }
-  case object Ruleset extends Prefix { override def toString() = "rls" }
-  case object ServiceLevel extends Prefix { override def toString() = "svl" }
-
-}
-
-object IdGenerator {
-
-  val Separator = "-"
-
-}
-
-case class IdGenerator(prefix: Prefix) {
+sealed trait IdGenerator {
 
   private[this] val random = new Random()
 
-  def randomId(): String = {
-    prefix.toString + IdGenerator.Separator + random.positiveLong()
-  }
-}
+  def prefix(): String
 
+  def randomId(): String = {
+    prefix.toString + IdGenerator.Separator + random.alphaNumeric(randomLength)
+  }
+
+  def length(): Int = IdGenerator.DefaultLength
+
+  private[util] def randomLength = {
+    length - prefix().length - IdGenerator.Separator.length
+  }
+
+}
--- test/io/flow/play/util/IdGeneratorSpec.scala
+++ test/io/flow/play/util/IdGeneratorSpec.scala
@@ -6,61 +6,104 @@ class IdGeneratorSpec extends FunSpec with Matchers {
 
   private[this] val SourcePath = "app/io/flow/play/util/IdGenerator.scala"
   private[this] val RequiredPrefixLength = 3
-  private[this] val MinimumPrefixesExpected = 5
-  private[this] val PrefixRegexp = """^case object (\w+) extends Prefix \{ override def toString\(\) = "(\w+)" \}$""".r
+  private[this] val MinimumRandomLength = 16
+  private[this] val MinimumGeneratorsExpected = 5
+  private[this] val IdGeneratorRegexp = """^case object (\w+) extends IdGenerator \{ override val prefix = "(\w+)" \}$""".r
 
-  private[this] case class PrefixValue(name: String, value: String)
+  private[this] case class ParsedIdGenerator(name: String, prefix: String)
+
+  private[this] val generators = Seq(
+    IdGenerator.Carrier, IdGenerator.Center, IdGenerator.Region, IdGenerator.Ruleset, IdGenerator.ServiceLevel
+  )
+
+  // Parse the source file to extract declared generators
+  private[this] def parseGenerators(failOnError: Boolean = false): Seq[ParsedIdGenerator] = {
+    scala.io.Source.fromFile(new java.io.File(SourcePath)).getLines.
+      filter { l => l.indexOf(" extends IdGenerator") > 0 }.
+      map(_.trim).
+      flatMap { l =>
+        l match {
+          case IdGeneratorRegexp(name, prefix) => {
+            Some(ParsedIdGenerator(name, prefix))
+          }
+          case _ => {
+            if (failOnError) {
+              sys.error(s"$SourcePath: IdGenerator object did not match expected format: $l")
+            }
+            None
+          }
+        }
+      }.toSeq
+  }
+
+  it("parsed generators non empty") {
+    if (parseGenerators().size < MinimumGeneratorsExpected) {
+      fail(s"$SourcePath: Expected to find at least $MinimumGeneratorsExpected prefixes but only found[${parseGenerators().size}]")
+    }
+  }
+
+  it("generators declared in standard format") {
+    parseGenerators(failOnError = true)
+  }
+
+  it("generators declares all") {
+    val missing = parseGenerators().find { g =>
+      !generators.map(_.prefix).contains(g.prefix)
+    }
+    if (!missing.isEmpty) {
+      fail("Test case does not document all IdGenerators. Missing: " + missing.map(_.name).mkString(", "))
+    }
+  }
 
   it("randomId") {
-    val generator = IdGenerator(Prefix.Carrier)
+    val generator = IdGenerator.Carrier
     val ids = 1.to(100).map { _ => generator.randomId() }
     ids.size should be(100)
     ids.distinct.size should be(ids.size)
   }
 
   it("randomId respects prefix") {
-    IdGenerator(Prefix.Carrier).randomId().startsWith(Prefix.Carrier.toString() + IdGenerator.Separator) should be(true)
-    IdGenerator(Prefix.Center).randomId().startsWith(Prefix.Center.toString() + IdGenerator.Separator) should be(true)
+    IdGenerator.Carrier.randomId().startsWith(IdGenerator.Carrier.prefix + IdGenerator.Separator) should be(true)
+    IdGenerator.Center.randomId().startsWith(IdGenerator.Center.prefix + IdGenerator.Separator) should be(true)
+  }
+
+  it("length is accurate") {
+    IdGenerator.Carrier.randomId().length should be(IdGenerator.Carrier.length())
+  }
+
+  it("randomLength exceeds minimum") {
+    generators.foreach { generator =>
+      if (generator.randomLength < MinimumRandomLength) {
+        fail(s"IdGenerator[${generator}] randomLength[${generator.randomLength}] is too short - minimum is $MinimumRandomLength")
+      }
+    }
+  }
+
+  it("generator names and values meet requirements") {
+    parseGenerators().foreach { gen =>
+      if (gen.prefix.toLowerCase != gen.prefix) {
+        fail(s"$SourcePath: IdGenerator[${gen.name}] prefix[${gen.prefix}] must be in lower case")
+      } else if (gen.prefix.trim != gen.prefix) {
+        fail(s"$SourcePath: IdGenerator[${gen.name}] prefix[${gen.prefix}] must be trimmed")
+      } else if (gen.prefix.length != RequiredPrefixLength) {
+        fail(s"$SourcePath: IdGenerator[${gen.name}] prefix[${gen.prefix}] must be exactly $RequiredPrefixLength characters")
+      } else if (gen.name.substring(0, 1) != gen.name.substring(0, 1).toUpperCase) {
+        fail(s"$SourcePath: IdGenerator[${gen.name}] must start with a capital letter")
+      }
+    }
   }
 
   it("All prefixes are unique and standard") {
-    val all = scala.io.Source.fromFile(new java.io.File(SourcePath)).getLines.
-      filter { l => l.indexOf(" extends Prefix") > 0 }.
-      map(_.trim).
-      map { l =>
-        l match {
-          case PrefixRegexp(name, value) => {
-            if (value.toLowerCase != value) {
-              fail(s"$SourcePath: Prefix[$name] value[$value] must be in lower case")
-          } else if (value.trim != value) {
-              fail(s"$SourcePath: Prefix[$name] value[$value] must be trimmed")
-            } else if (value.length != RequiredPrefixLength) {
-              fail(s"$SourcePath: Prefix[$name] value[$value] must be exactly $RequiredPrefixLength characters")
-            } else if (name.substring(0, 1) != name.substring(0, 1).toUpperCase) {
-              fail(s"$SourcePath: Prefix[$name] must start with a capital letter")
-            }
-            PrefixValue(name, value)
-          }
-          case _ => {
-            fail(s"$SourcePath: Prefix object did not match expected format: $l")
-          }
-        }
-      }.toList
-
-    if (all.size < MinimumPrefixesExpected) {
-      fail(s"$SourcePath: Expected to find at least $MinimumPrefixesExpected prefixes but only found[${all.size}]")
-    }
-
-    all.groupBy(_.value).foreach { case (value, prefixes) =>
+    parseGenerators().groupBy(_.prefix).foreach { case (prefix, prefixes) =>
       if (prefixes.length > 1) {
-        fail(s"$SourcePath: Prefix[$value] is repeated: " + prefixes.map(_.name).mkString(", "))
+        fail(s"$SourcePath: IdGenerator prefix[$prefix] is repeated: " + prefixes.map(_.name).mkString(", "))
       }
     }
 
-    val actual = all.map(_.name)
-    val sorted = all.map(_.name).sorted
+    val actual = parseGenerators().map(_.name)
+    val sorted = parseGenerators().map(_.name).sorted
     if (actual != sorted) {
-      fail(s"$SourcePath: Prefixes must be sorted alphabetically. Expected order to be[${sorted.mkString(", ")}] but found[${actual.mkString(", ")}]")
+      fail(s"$SourcePath: IdGenerators must be sorted alphabetically. Expected order to be[${sorted.mkString(", ")}] but found[${actual.mkString(", ")}]")
     }
   }
 

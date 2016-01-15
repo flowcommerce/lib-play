e38f2e0d23ffde46bbcac773786673dd766b661a
mikeclimbrock <mikeroth@flow.io>
2016-01-15 12:12:45 -0500

    add withJsValue wrapper

--- app/io/flow/play/util/FormDataHelper.scala
+++ app/io/flow/play/util/FormDataHelper.scala
@@ -1,9 +1,50 @@
 package io.flow.play.util
 
 import play.api.libs.json.{JsObject, Json, JsValue}
+import play.api.mvc.{Result, AnyContent}
+import play.api.mvc.Results._
+import scala.concurrent.Future
 
 
 object FormDataHelper {
+  def withJsValue(
+   contentType: Option[String],
+   body: AnyContent
+  ) (
+   function: JsValue => Future[Result]
+  ): Future[Result] = {
+    contentType match {
+      case Some("application/x-www-form-urlencoded") =>
+        function(
+          body.asFormUrlEncoded.map(formDataToJson(_)).getOrElse(Json.obj())
+        )
+      case Some("application/json") =>
+        function(
+          body.asJson match {
+            case Some(json) => json
+            case None => Json.toJson(Validation.invalidJsonDocument())
+          }
+        )
+      case Some(ct) =>
+        Future {
+          UnprocessableEntity(
+            Json.toJson(
+              Validation.error(s"Unsupported Content-Type, [$ct]. Must be 'application/x-www-form-urlencoded' or 'application/json'")
+            )
+          )
+        }
+      case None =>
+        Future {
+          // TODO: What do you think about inferring the type if not specified?
+          UnprocessableEntity(
+            Json.toJson(
+              Validation.error(s"Missing Content-Type Header. Must be 'application/x-www-form-urlencoded' or 'application/json'")
+            )
+          )
+        }
+    }
+  }
+
   def convertFormDataValuesToJson(data: Map[String, Seq[String]]): Map[String, JsValue] = {
     data.map { case (key, value) =>
       key -> (value match {

34274e51ca93b39815886d1e641860d4863789bf
Michael Bryzek <mbryzek@alum.mit.edu>
2016-02-20 17:15:20 -0500

    Fix parsing error

---
+++ test/io/flow/play/util/FlowEnvironmentSpec.scala
@@ -0,0 +1,24 @@
+package io.flow.play.util
+
+import org.scalatest.{FunSpec, Matchers}
+
+class FlowEnvironmentSpec extends FunSpec with Matchers {
+
+  it("fromString") {
+    FlowEnvironment.fromString("development") should be(Some(FlowEnvironment.Development))
+    FlowEnvironment.fromString("production") should be(Some(FlowEnvironment.Production))
+  }
+
+  it("Current is defined") {
+    FlowEnvironment.all.contains(FlowEnvironment.Current) should be(true)
+  }
+
+  it("parse") {
+    FlowEnvironment.parse("test", "development") should be(FlowEnvironment.Development)
+    FlowEnvironment.parse("test", "production") should be(FlowEnvironment.Production)
+    intercept[Throwable] {
+      FlowEnvironment.parse("test", "other")
+    }.getMessage should be("Value[other] from test[env] is invalid. Valid values are: development, production")
+  }
+
+}

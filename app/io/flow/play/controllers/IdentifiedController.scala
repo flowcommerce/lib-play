a0e5158faf1b13b9a5fb30a52d46280b2e797755
Chris Hicks <chicks@flow.io>
2016-03-10 17:49:08 -0500

    Return user references instead of user in controllers

--- app/io/flow/play/controllers/IdentifiedController.scala
+++ app/io/flow/play/controllers/IdentifiedController.scala
@@ -1,6 +1,6 @@
 package io.flow.play.controllers
 
-import io.flow.common.v0.models.User
+import io.flow.common.v0.models.{UserReference, User}
 import play.api.mvc.Results.Unauthorized
 import scala.concurrent.Future
 import play.api.mvc._
@@ -15,28 +15,25 @@ trait IdentifiedController extends AnonymousController {
   def unauthorized[A](request: Request[A]): Result = Unauthorized
 
   class IdentifiedRequest[A](
-    val user: User,
+    val user: UserReference,
     request: Request[A]
   ) extends WrappedRequest[A](request)
 
   object Identified extends ActionBuilder[IdentifiedRequest] {
 
     def invokeBlock[A](request: Request[A], block: (IdentifiedRequest[A]) => Future[Result]) = {
-      user(request.session, request.headers, request.path, request.queryString).flatMap { userOption =>
-        userOption match {
-          case None => {
-            Future { unauthorized(request) }
-          }
-          case Some(user) => {
-            block(
-              new IdentifiedRequest(user, request)
-            )
-          }
+      user(request.session, request.headers, request.path, request.queryString).flatMap {
+        case None => {
+          Future { unauthorized(request) }
+        }
+        case Some(user) => {
+          block(
+            new IdentifiedRequest(user, request)
+          )
         }
       }
     }
   }
-
 }
 
 trait IdentifiedRestController extends IdentifiedController with UserFromBasicAuthorizationToken

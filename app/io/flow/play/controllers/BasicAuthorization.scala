30419bbf5be92586f76315bd96d9090ccb7d1c45
Chris Hicks <chicks@flow.io>
2016-03-10 10:13:41 -0500

    Parse JWT tokens from basic auth header

--- app/io/flow/play/controllers/BasicAuthorization.scala
+++ app/io/flow/play/controllers/BasicAuthorization.scala
@@ -2,27 +2,51 @@ package io.flow.play.controllers
 
 import org.apache.commons.codec.binary.Base64
 
+import authentikat.jwt._
+
 object BasicAuthorization {
 
   trait Authorization
   case class Token(token: String) extends Authorization
+  case class JWTToken(userId: String) extends Authorization
 
   def get(value: Option[String]): Option[Authorization] = {
-    value.flatMap { get(_) }
+    value.flatMap { get }
   }
 
   /**
-   * Parses the actual authorization header value
+    * Parses the actual authorization header value. Acceptable types are:
+    * - Basic - the API Token for the user.
+    * - Bearer - the JWT Token for the user with that contains an id field representing the user id in the database
    */
-  def get(value: String): Option[Authorization] = {
-    value.split(" ").toList match {
+  def get(headerValue: String): Option[Authorization] = {
+    headerValue.split(" ").toList match {
       case "Basic" :: value :: Nil => {
         new String(Base64.decodeBase64(value.getBytes)).split(":").toList match {
           case Nil => None
           case token :: rest => Some(Token(token))
         }
       }
+
+      case "Bearer" :: value :: Nil => {
+        value match {
+          case JsonWebToken(header, claimsSet, signature) => createJWTToken(claimsSet)
+          case _ => None
+        }
+      }
       case _ => None
+    }
+  }
+
+  private[this] def createJWTToken(claimsSet: JwtClaimsSetJValue): Option[JWTToken] = {
+    claimsSet.asSimpleMap.toOption match {
+      case Some(claims) =>
+        println(s"got claims! ${claims}")
+        claims.get("id").map(JWTToken)
+      case _ => {
+        println("got nothing back?")
+        None
+      }
     }
   }
 
---
+++ test/io/flow/play/controllers/BasicAuthorizationSpec.scala
@@ -0,0 +1,38 @@
+package io.flow.play.controllers
+
+import io.flow.play.controllers.BasicAuthorization.{Token, JWTToken}
+import org.scalatest.{Matchers, FunSpec}
+
+
+class BasicAuthorizationSpec extends FunSpec with Matchers {
+  val jwtSalt = "development-$342-salt"
+
+  describe("BasicAuthorization") {
+    describe("Basic") {
+      it("should decode a basic auth header") {
+        val headerValue = "Basic YWRtaW46"
+
+        BasicAuthorization.get(headerValue).map { authToken =>
+          authToken match {
+            case Token(token) => token shouldBe "admin"
+            case _ => fail("Did not parse a Token, got a different type instead.")
+          }
+        }.getOrElse(fail("Could not parse token!"))
+      }
+    }
+
+    describe("JWT") {
+      it("should decode a basic auth bearer header") {
+        val headerValue = "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6InVzci0yMDE2MDEzMC0xIiwiaWF0IjoxNDU3NjIyMzg2LCJleHAiOjE0NTc3MDg3ODYsImlzcyI6Imh0dHA6Ly9sb2NhbGhvc3QiLCJzdWIiOiJ0ZXN0dXNlckB0ZXN0LmNvbSJ9.v9HkqkKEaiq88T6QjLkpiKW2mjM_mqqg2owW_SzxjCw"
+
+        BasicAuthorization.get(headerValue).map { authToken =>
+          authToken match {
+            case JWTToken(userId) => userId shouldBe "usr-20160130-1"
+            case _ => fail("Did not parse a JWTToken, got a different type instead.")
+          }
+        }.getOrElse(fail("Could not parse token!"))
+      }
+    }
+  }
+
+}

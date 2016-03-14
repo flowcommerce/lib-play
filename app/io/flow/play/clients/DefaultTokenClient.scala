da9a08e4340d553e4a14027565aa6c8d9ccb0fb3
Michael Bryzek <mbryzek@alum.mit.edu>
2016-03-14 10:50:52 -0400

    Remove dependency on registry from token client

--- app/io/flow/play/clients/DefaultTokenClient.scala
+++ app/io/flow/play/clients/DefaultTokenClient.scala
@@ -1,7 +1,10 @@
 package io.flow.play.clients
 
+/**
+  * Gets an instance of the token client. Note that we cannot declare
+  * a dependency on registry here as registry requires token to
+  * authorize the user. Thus we create an instance of the token client
+  * using the default base URL declared in apidoc.
+  */
 @javax.inject.Singleton
-class DefaultTokenClient @javax.inject.Inject() (
-  registry: Registry
-) extends io.flow.token.v0.Client(registry.host("token"))
-
+class DefaultTokenClient @javax.inject.Inject() () extends io.flow.token.v0.Client()
--- app/io/flow/play/clients/Registry.scala
+++ app/io/flow/play/clients/Registry.scala
@@ -34,8 +34,23 @@ trait RegistryConstants {
   val ProductionDomain = "api.flow.io"
   val TokenVariableName = "io.flow.user.token"
 
+  /**
+    * The resolved name of the development host. At Flow, this is the
+    * alias 'vm' which we expect to be setup in /etc/hosts to point to
+    * the IP address of the VM running our docker containers locally.
+    */
+  val DevelopmentHost = "vm"
+  
   def log(env: String, applicationId: String, message: String) {
     Logger.info(s"[${getClass.getName} $env] app[$applicationId] $message")
+  }
+
+  /**
+    * Returns the hostname of the specified application in the
+    * production environment.
+    */
+  def productionHost(applicationId: String): String = {
+    s"https://${applicationId}.${ProductionDomain}"
   }
 
 }
@@ -45,37 +60,47 @@ trait RegistryConstants {
   */
 class ProductionRegistry() extends Registry with RegistryConstants {
 
-  override def host(applicationId: String): String = {
-    val host = s"http://${applicationId}.${ProductionDomain}"
+  override def host(applicationId: String) = {
+    val host = productionHost(applicationId)
     log("Production", applicationId, s"Host[$host]")
     host
   }
 
 }
 
-/**
-  * This trait fetches a registry Application - and from there
-  * provides the full host using the external port for that
-  * application.
-  */
-trait RegistryApplicationProvider extends Registry with RegistryConstants {
+@javax.inject.Singleton
+class DevelopmentRegistry @javax.inject.Inject() (
+  app: play.api.Application,
+  config: Config
+) extends Registry with RegistryConstants {
 
-  /**
-    * Name of an environment variable containing the name of the VM
-    * Host. This is used for local development and should resolve to
-    * the VM of the virtual machine running the docker containers.
-    */
-  val DevHostName = "io.flow.dev.host"
+  private[this] lazy val RegistryHost: String = {
+    val applicationId = "registry"
+    val varName = overriddeVariableName(applicationId)
 
-  /**
-    * The resolved name of the development host
-    */
-  lazy val DevHost = config.optionalString(DevHostName).getOrElse("vm")
-
-  override def host(applicationId: String): String = {
     overridden(applicationId) match {
       case Some(host) => {
-        log("Development", applicationId, s"Host[$host] (overridden via env var[${overriddeVariableName(applicationId)}]")
+        log("Development", applicationId, s"Host[$host] (overridden via env var[$varName]")
+        host
+      }
+
+      case None => {
+        val host = productionHost(applicationId)
+        log("Development", applicationId, s"Host[$host] (can override via env var[$varName]")
+        host
+      }
+    }
+  }
+
+  private[this] lazy val token = config.requiredString(TokenVariableName)
+  private[this] lazy val client = new Client(RegistryHost, Some(Authorization.Basic(token)))
+
+  override def host(applicationId: String): String = {
+    val varName = overriddeVariableName(applicationId)
+
+    overridden(applicationId) match {
+      case Some(host) => {
+        log("Development", applicationId, s"Host[$host] (overridden via env var[$varName]")
         host
       }
 
@@ -83,16 +108,27 @@ trait RegistryApplicationProvider extends Registry with RegistryConstants {
         val port = getById(applicationId).ports.headOption.getOrElse {
           sys.error(s"application[$applicationId] does not have any ports in registry")
         }
-        val host = s"http://${DevHost}:${port.external}"
-        log("Development", applicationId, s"Host[$host] (can override via env var[${overriddeVariableName(applicationId)}]")
+        val host = s"http://${DevelopmentHost}:${port.external}"
+        log("Development", applicationId, s"Host[$host] (can override via env var[$varName]")
         host
       }
     }
   }
 
-  def config: Config
+  def getById(applicationId: String): Application = {
+    import scala.concurrent.ExecutionContext.Implicits.global
 
-  def getById(applicationId: String): Application
+    Await.result(
+      client.applications.getById(applicationId).map { app =>
+        app
+      }.recover {
+        case UnitResponse(401) => sys.error(s"Unauthorized to fetch application[$applicationId] from registry at $RegistryHost")
+        case UnitResponse(404) => sys.error(s"application[$applicationId] not found in registry at $RegistryHost")
+        case ex: Throwable => throw new Exception(s"ERROR connecting to registry at $RegistryHost", ex)
+      }
+      , Duration(5, "seconds")
+    )
+  }
 
   /**
     * Allows user to set an environment variable to specify the
@@ -112,45 +148,8 @@ trait RegistryApplicationProvider extends Registry with RegistryConstants {
 }
 
 @javax.inject.Singleton
-class DevelopmentRegistry @javax.inject.Inject() (val config: Config) extends RegistryApplicationProvider {
-  import scala.concurrent.ExecutionContext.Implicits.global
-
-  private[this] lazy val RegistryHost = {
-    overridden("registry") match {
-      case None => {
-        val host = s"http://registry.$ProductionDomain"
-        log("Development", "registry", s"Host[$host] (can override via env var[${overriddeVariableName("registry")}])")
-        host
-      }
-      case Some(host) => {
-        log("Development", "registry", s"Host[$host] (overridden via env var[${overriddeVariableName("registry")}]")
-        host
-      }
-    }
-  }
-
-  private[this] lazy val token = config.requiredString(TokenVariableName)
-
-  private[this] lazy val client = new Client(RegistryHost, Some(Authorization.Basic(token)))
-
-  override def getById(applicationId: String): Application = {
-    Await.result(
-      client.applications.getById(applicationId).map { app =>
-        app
-      }.recover {
-        case UnitResponse(401) => sys.error(s"Unauthorized to fetch application[$applicationId] from registry at $RegistryHost")
-        case UnitResponse(404) => sys.error(s"application[$applicationId] not found in registry at $RegistryHost")
-        case ex: Throwable => throw new Exception(s"ERROR connecting to registry at $RegistryHost", ex)
-      }
-      , Duration(5, "seconds")
-    )
-  }
-
-}
-
-@javax.inject.Singleton
 class MockRegistry() extends Registry {
 
-  override def host(applicationId: String) = "http://localhost"
+  override def host(applicationId: String) = s"http://$applicationId.localhost"
 
 }

e80d4b73eb95d88d3fff2fe34f443c43641d77b7
Michael Bryzek <mbryzek@alum.mit.edu>
2016-03-14 11:28:01 -0400

    Avoid circular dependency on registry

--- app/io/flow/play/clients/DefaultTokenClient.scala
+++ app/io/flow/play/clients/DefaultTokenClient.scala
@@ -4,7 +4,10 @@ package io.flow.play.clients
   * Gets an instance of the token client. Note that we cannot declare
   * a dependency on registry here as registry requires token to
   * authorize the user. Thus we create an instance of the token client
-  * using the default base URL declared in apidoc.
+  * but explicitly set the port to known values for both development
+  * and production.
   */
 @javax.inject.Singleton
-class DefaultTokenClient @javax.inject.Inject() () extends io.flow.token.v0.Client()
+class DefaultTokenClient @javax.inject.Inject() () extends io.flow.token.v0.Client(
+  RegistryConstants.host("token", 6151)
+)

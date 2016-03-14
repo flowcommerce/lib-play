4224edd60935f1fe1b143e1b3785fe2498388d0c
Michael Bryzek <mbryzek@alum.mit.edu>
2016-03-13 20:56:39 -0400

    Modify mock config to default to default config

--- app/io/flow/play/clients/MockConfig.scala
+++ app/io/flow/play/clients/MockConfig.scala
@@ -1,14 +1,14 @@
 package io.flow.play.clients
 
-import io.flow.play.util.Config
+import io.flow.play.util.{Config, DefaultConfig}
 
 @javax.inject.Singleton
-case class MockConfig @javax.inject.Inject() () extends Config {
+case class MockConfig @javax.inject.Inject() (defaultConfig: DefaultConfig) extends Config {
 
   override def optionalString(name: String): Option[String] = {
     name match {
       case "JWT_SALT" => Some("test")
-      case _ => None
+      case _ => defaultConfig.optionalString(name)
     }
   }
 

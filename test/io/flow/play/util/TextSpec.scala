d9d60e6782357aabb0c7264f1167f7c01ba8d357
Michael Bryzek <mbryzek@alum.mit.edu>
2016-06-22 16:14:34 -0400

    Add Text class w/ truncate

---
+++ test/io/flow/play/util/TextSpec.scala
@@ -0,0 +1,22 @@
+package io.flow.play.util
+
+import org.scalatest.{FunSpec, Matchers}
+
+class TextSpec extends FunSpec with Matchers {
+
+  describe("truncate") {
+
+    it("does not truncate short text") {
+      Text.truncate("foo") should be("foo")
+      Text.truncate("This is") should be("This is")
+    }
+
+    it("respects limit") {
+      Text.truncate("1234567890", 10) should be("1234567890")
+      Text.truncate("12345678900", 10) should be("1234567...")
+      Text.truncate("This is a long sentence", 50) should be("This is a long sentence")
+      Text.truncate("This is a long sentence", 10) should be("This is...")
+    }
+  }
+
+}

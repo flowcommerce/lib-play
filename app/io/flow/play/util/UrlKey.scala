47d94197042f675806fa05dde520a164d5d3161a
Michael Bryzek <mbryzek@alum.mit.edu>
2016-06-22 22:01:04 -0400

    Add test case, and fix underlying issue w/ random lib

--- app/io/flow/play/util/UrlKey.scala
+++ app/io/flow/play/util/UrlKey.scala
@@ -52,10 +52,12 @@ case class UrlKey(
     implicit checkFunction: String => Boolean = { _ => true }
   ): String = {
     val formatted = format(value)
+
     (formatted.length < minKeyLength) match {
       case true => {
         generate(formatted + random.alphaNumeric(minKeyLength - formatted.length), suffix)(checkFunction)
       }
+
       case false => {
         val (key, nextSuffix) = suffix match {
           case None => (formatted, 1)

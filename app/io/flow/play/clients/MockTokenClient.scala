a55182cd8615b9b7d56be5463ceaed724b3404d5
Paolo Lim <paolo@flow.io>
2016-07-06 17:33:24 -0400

    Regen lib-play and update MockTokenClient (#71)

--- app/io/flow/play/clients/MockTokenClient.scala
+++ app/io/flow/play/clients/MockTokenClient.scala
@@ -1,8 +1,8 @@
 package io.flow.play.clients
 
-import io.flow.common.v0.models.{User, UserReference}
 import io.flow.token.v0.mock
-import io.flow.token.v0.models.Token
+import io.flow.token.v0.models.TokenReference
+
 import scala.concurrent.{ExecutionContext, Future}
 
 /**
@@ -11,14 +11,14 @@ import scala.concurrent.{ExecutionContext, Future}
   */
 case class MockTokenData() {
 
-  case class InternalToken(key: String, token: Token)
+  case class InternalToken(key: String, token: TokenReference)
 
   var tokens = scala.collection.mutable.ListBuffer[InternalToken]()
 
   /**
     * @param key: The actual token
     */
-  def add(key: String, token: Token) = {
+  def add(key: String, token: TokenReference) = {
     tokens += InternalToken(key, token)
   }
 
@@ -38,7 +38,7 @@ case class MockTokens(data: MockTokenData) extends mock.MockTokens {
   override def get(
     token: Seq[String],
     requestHeaders: Seq[(String, String)] = Nil
-  )(implicit ec: scala.concurrent.ExecutionContext): scala.concurrent.Future[Seq[io.flow.token.v0.models.Token]] = scala.concurrent.Future {
+  )(implicit ec: ExecutionContext): scala.concurrent.Future[Seq[TokenReference]] = Future {
     data.tokens.filter { t =>
       token.contains(t.key)
     }.map(_.token)
@@ -47,10 +47,8 @@ case class MockTokens(data: MockTokenData) extends mock.MockTokens {
   override def getByToken(
     token: String,
     requestHeaders: Seq[(String, String)] = Nil
-  )(implicit ec: scala.concurrent.ExecutionContext): scala.concurrent.Future[io.flow.token.v0.models.Token] = {
-    get(Seq(token)).map { _.headOption.getOrElse {
-      throw new io.flow.token.v0.errors.UnitResponse(404)
-    }}
+  )(implicit ec: ExecutionContext): scala.concurrent.Future[TokenReference] = get(Seq(token)).map { t =>
+    t.headOption.get
   }
 
 }

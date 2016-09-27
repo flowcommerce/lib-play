71653902bbc64ec0629d1a6516ceec66c3dfb9e8
Michael Bryzek <mbryzek@alum.mit.edu>
2016-09-27 15:18:04 -0400

    Upgrade to errors model

--- app/io/flow/play/util/Validation.scala
+++ app/io/flow/play/util/Validation.scala
@@ -1,59 +1,30 @@
 package io.flow.play.util
 
-import io.flow.common.v0.models.Error
-import play.api.libs.json.{Json, JsError}
-
-trait Validated {
-
-  def errors: Seq[Error]
-
-  def assertValid() {
-    assert(errors.isEmpty, errors.map(_.message).mkString(" and "))
-  }
-
-}
+import io.flow.error.v0.models.ValidationError
+import play.api.libs.json.JsError
 
 object Validation {
 
-  object Codes {
-    val InvalidJson = "invalid_json"
-    val InvalidSort = "invalid_sort"
-    val UserAuthorizationFailed = "user_authorization_failed"
-    val Error = "validation_error"
-    val ClientError = "client_error"
-    val ServerError = "server_error"
+  def invalidJson(errors: JsError): ValidationError = {
+    error(errors.toString)
   }
 
-  def invalidJson(errors: JsError): Seq[Error] = {
-    Seq(Error(Codes.InvalidJson, errors.toString))
+  def invalidJsonDocument(): ValidationError = {
+    error("Content is not valid JSON")
   }
 
-  def invalidJsonDocument(): Seq[Error] = {
-    Seq(Error(Codes.InvalidJson, "Content is not valid JSON"))
+  def invalidSort(messages: Seq[String]): ValidationError = {
+    errors(messages)
   }
 
-  def invalidSort(errors: Seq[String]): Seq[Error] = {
-    errors.map { error => Error(Codes.InvalidSort, error) }
-  }
-
-  def userAuthorizationFailed(): Seq[Error] = {
-    Seq(Error(Codes.UserAuthorizationFailed, "Email address and/or password did not match"))
-  }
-
-  def error(message: String): Seq[Error] = {
+  def error(message: String): ValidationError = {
     errors(Seq(message))
   }
 
-  def errors(messages: Seq[String]): Seq[Error] = {
-    messages.map { msg => Error(Codes.Error, msg) }
+  def errors(messages: Seq[String]): ValidationError = {
+    ValidationError(
+      messages = messages
+    )
   }
 
-  def clientError(error: String = "Client Error"): Seq[Error] = {
-    Seq(Error(Codes.ClientError, error))
-  }
-
-  def serverError(error: String = "Internal Server Error"): Seq[Error] = {
-    Seq(Error(Codes.ServerError, error))
-  }
-  
 }

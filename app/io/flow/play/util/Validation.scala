7a2856a05ad0ef57b94e13f4a5ca9106405dfe5d
Michael Bryzek <mbryzek@alum.mit.edu>
2016-09-28 14:24:38 -0400

    Upgrade to GenericError

--- app/io/flow/play/util/Validation.scala
+++ app/io/flow/play/util/Validation.scala
@@ -1,28 +1,28 @@
 package io.flow.play.util
 
-import io.flow.error.v0.models.ValidationError
+import io.flow.error.v0.models.GenericError
 import play.api.libs.json.JsError
 
 object Validation {
 
-  def invalidJson(errors: JsError): ValidationError = {
+  def invalidJson(errors: JsError): GenericError = {
     error(errors.toString)
   }
 
-  def invalidJsonDocument(): ValidationError = {
+  def invalidJsonDocument(): GenericError = {
     error("Content is not valid JSON")
   }
 
-  def invalidSort(messages: Seq[String]): ValidationError = {
+  def invalidSort(messages: Seq[String]): GenericError = {
     errors(messages)
   }
 
-  def error(message: String): ValidationError = {
+  def error(message: String): GenericError = {
     errors(Seq(message))
   }
 
-  def errors(messages: Seq[String]): ValidationError = {
-    ValidationError(
+  def errors(messages: Seq[String]): GenericError = {
+    GenericError(
       messages = messages
     )
   }

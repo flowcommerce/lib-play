82b58102d7152a02e0199a8339f94bc11641d9d8
Ben <benwaffle@users.noreply.github.com>
2017-06-29 12:35:37 -0400

    Add cache (#160)
    * Add cache
    
    * Remove commented code
    
    * cache should return old value if refresh() fails
    
    * try 2
    
    * Update cache to make more explicit what is happening on stale data
    
    * Use failure function

---
+++ app/io/flow/play/util/CacheWithFallbackToStaleDataSpec.scala
@@ -0,0 +1,84 @@
+package io.flow.play.util
+
+import org.joda.time.DateTime
+import play.api.Logger
+
+import scala.concurrent.duration._
+import scala.util.{Failure, Success, Try}
+
+private[util] case class CacheEntry[V](
+  value: V,
+  expiresAt: DateTime
+) {
+
+  def isExpired: Boolean = expiresAt.isBeforeNow
+
+}
+
+/**
+  * Caches data for a short period of time (configurable, defaults to 1 minute)
+  *
+  * Refreshes data on demand (when you call `get`, if entry is not in cache
+  * executes the refresh function then). If the call to `get` failes, and
+  * and there is data cached in memory, you will get back the stale data.
+  */
+trait CacheWithFallbackToStaleData[K, V] {
+
+  private[this] val cache = new java.util.concurrent.ConcurrentHashMap[K, CacheEntry[V]]()
+
+  /**
+    * Defines how long to cache each value for
+    */
+  val duration: FiniteDuration = FiniteDuration(1, MINUTES)
+
+  def refresh(key: K): V
+
+  def flush(key: K): Unit = {
+    cache.remove(key)
+  }
+
+  def get(key: K): V = {
+    Option(cache.get(key)) match {
+      case Some(entry) if !entry.isExpired => entry.value
+
+      case Some(staleEntry) => {
+        doRefresh(key) { ex =>
+          Logger.warn(s"Cache[${this.getClass.getName}] key[$key]: Falling back to stale data as refresh failed with: ${ex.getMessage}", ex)
+          staleEntry.value
+        }
+      }
+
+      case None => {
+        doRefresh(key) { ex =>
+          val msg = s"FlowError for Cache[${this.getClass.getName}] key[$key]: ${ex.getMessage}"
+          Logger.error(msg, ex)
+          sys.error(msg)
+        }
+      }
+    }
+  }
+
+  private[this] def doRefresh(key: K)(
+    failureFunction: Throwable => V
+  ): V = {
+    Try {
+      refresh(key)
+    } match {
+      case Success(value) => {
+        cache.put(
+          key,
+          CacheEntry(
+            value = value,
+            expiresAt = DateTime.now.plusSeconds(duration.toSeconds.toInt)
+          )
+        )
+        value
+      }
+
+      case Failure(ex) => {
+        failureFunction(ex)
+      }
+    }
+  }
+
+}
---
+++ test/io/flow/play/util/CacheWithFallbackToStaleDataSpec.scala
@@ -0,0 +1,77 @@
+package io.flow.play.util
+
+import org.scalatestplus.play.{OneAppPerSuite, PlaySpec}
+import org.scalatest.concurrent.Eventually.{eventually, timeout}
+import org.scalatest.time.{Seconds, Span}
+
+import scala.concurrent.duration._
+
+class CacheWithFallbackToStaleDataSpec extends PlaySpec with OneAppPerSuite {
+
+  private[this] case class TestCacheWithFallbackToStaleData() extends CacheWithFallbackToStaleData[String, String] {
+    private[this] val data = scala.collection.mutable.Map[String, String]()
+    var numberRefreshes: Int = 0
+    var refreshShouldFail = false
+
+    override val duration: FiniteDuration = FiniteDuration(1, SECONDS)
+
+    override def refresh(key: String): String = {
+      if (refreshShouldFail) {
+        throw new Exception("test refresh() failing on purpose")
+      } else {
+        numberRefreshes += 1
+        data.getOrElse(key, sys.error("Missing test data for key[$key]"))
+      }
+    }
+
+    def add(key: String, value: String): Unit = {
+      data += (key -> value)
+    }
+  }
+
+  private[this] def eventuallyInNSeconds[T](n: Int)(f: => T): T = {
+    eventually(timeout(Span(n, Seconds))) {
+      f
+    }
+  }
+
+  "cached values are served" in {
+    val cache = TestCacheWithFallbackToStaleData()
+    cache.add("a", "apple")
+
+    // Test cache hit
+    cache.get("a") must equal("apple")
+    cache.get("a") must equal("apple")
+    cache.numberRefreshes must equal(1)
+
+    // Test cache miss and auto refresh after 1 second
+    eventuallyInNSeconds(2) {
+      cache.get("a") must equal("apple")
+      cache.numberRefreshes must equal(2)
+    }
+  }
+
+  "supports multiple keys" in {
+    val cache = TestCacheWithFallbackToStaleData()
+    cache.add("a", "apple")
+    cache.add("p", "pear")
+
+    cache.get("a") must equal("apple")
+    cache.get("p") must equal("pear")
+  }
+
+  "failed refresh handled gracefully" in {
+    val cache = TestCacheWithFallbackToStaleData()
+    cache.add("a", "apple")
+    cache.get("a") must equal("apple")
+
+    cache.refreshShouldFail = true
+    cache.add("a", "not apple")
+
+    Thread.sleep(2000)
+
+    // failing refresh should return old value
+    cache.get("a") must equal("apple")
+  }
+
+}

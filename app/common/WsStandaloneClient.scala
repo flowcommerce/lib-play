fbdd453b6c9de63710bd210d50745baed9692f8a
Alex <alex@flow.io>
2017-09-07 23:56:36 -0400

    code moved out - can be deleted

--- app/common/WsStandaloneClient.scala
+++
@@ -1,8 +0,0 @@
-package common
-
-import play.api.libs.ws.ahc.{AhcWSClient, StandaloneAhcWSClient}
-import play.shaded.ahc.org.asynchttpclient.DefaultAsyncHttpClient
-
-trait WsStandaloneClient extends CommonDefaults {
-  val wsClient = new AhcWSClient(new StandaloneAhcWSClient(new DefaultAsyncHttpClient))
-}

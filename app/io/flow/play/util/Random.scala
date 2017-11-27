b46b8839b3bcbb2bf9c16b82ba47b13ea7c1cf4f
Jean Logeart <jean.logeart@gmail.com>
2017-11-27 14:31:54 -0500

    Add refreshing cache
    Add object helper
    
    Add small style improvements
    
    Revert "Optimize Random"
    
    This reverts commit e5520b09295928ac2fec7a963c6140858b0d6547.
    
    Rename method
    
    Add documentation
    
    Cleanup
    
    Improve text
    
    Cache anything
    
    Update name

--- app/io/flow/play/util/Random.scala
+++ app/io/flow/play/util/Random.scala
@@ -7,31 +7,31 @@ package io.flow.play.util
   */
 case class Random() {
 
-  import Random._
-
   private[this] val random = new java.security.SecureRandom
+
+  private[this] val Ambiguous = "B8G6I1l0OoQDS5Z2".split("")
+  private[this] val Numbers = "0123456789"
+  private[this] val Lower = "abcdefghijklmnopqrstuvwxyz"
+  private[this] val LowerAndUpper = Lower + Lower.toUpperCase
+  private[this] val LowerAndUpperAndNumbers = LowerAndUpper + Numbers
+  private[this] val NonAmbiguousLowerAndUpper = LowerAndUpper.split("").filter(!Ambiguous.contains(_)).mkString("")
+  private[this] val NonAmbiguousLowerAndUpperAndNumbers = NonAmbiguousLowerAndUpper + "3479"
 
   /**
     * Generate a random string of length n from the given alphabet
-    *
-    * @param alphabet The complete set of
+    * 
+    * @param alphabet The complete set of 
     * @param n Length of random string to generate
     */
   def string(alphabet: String)(n: Int): String = {
     assert(n > 0, "n must be > 0")
-    val s = new StringBuilder(n)
-    var i = n
-    while (i > 0) {
-      s.append(alphabet(random.nextInt(alphabet.length)))
-      i -= 1
-    }
-    s.toString()
+    Stream.continually(random.nextInt(alphabet.length)).map(alphabet).take(n).mkString
   }
 
   /**
     * Generate a random string of length n using only a-z (lower case
     * alphabet letters)
-    *
+    * 
     * @param n Length of random string to generate
     */
   def lowercaseAlpha(n: Int): String = {
@@ -41,7 +41,7 @@ case class Random() {
   /**
     * Generate a random string of length n using only a-z and A-Z
     * (alphabet letters only)
-    *
+    * 
     * @param n Length of random string to generate
     */
   def alpha(n: Int): String = {
@@ -54,44 +54,38 @@ case class Random() {
     * will start with a letter, not a number - this is mostly to
     * prevent problems with any applications that infer numeric based
     * on first digit (or strip zeroes).
-    *
+    * 
     * The random string is guaranteed to start with a letter (we do
     * this to avoid confusion in some programs like excel which can
     * infer a numeric type based on the first character)
-    *
+    * 
     * @param n Length of random string to generate
     */
   def alphaNumeric(n: Int): String = {
     if (n == 1) {
       alpha(1)
     } else {
-      val s = new StringBuilder(n)
-      s.append(alpha(1))
-      s.append(string(LowerAndUpperAndNumbers)(n - 1))
-      s.toString()
+      alpha(1) + string(LowerAndUpperAndNumbers)(n - 1)
     }
   }
 
-  /**
+    /**
     * Generate a random string of length n using only letters and
     * numbers that are non ambiguous (e.g. B can look like an 8 so
     * neither B nor 8 is used in the random string). This is a good
     * option for random strings that will be read by humans.
-    *
+    * 
     * The random string is guaranteed to start with a letter (we do
     * this to avoid confusion in some programs like excel which can
     * infer a numeric type based on the first character)
-    *
+    * 
     * @param n Length of random string to generate
     */
   def alphaNumericNonAmbiguous(n: Int): String = {
     if (n == 1) {
       string(NonAmbiguousLowerAndUpper)(1)
     } else {
-      val s = new StringBuilder(n)
-      s.append(string(NonAmbiguousLowerAndUpper)(1))
-      s.append(string(NonAmbiguousLowerAndUpperAndNumbers)(n - 1))
-      s.toString()
+      string(NonAmbiguousLowerAndUpper)(1) + string(NonAmbiguousLowerAndUpperAndNumbers)(n - 1)
     }
   }
 
@@ -100,7 +94,7 @@ case class Random() {
     */
   @scala.annotation.tailrec
   final def positiveInt(): Int = {
-    val value = random.nextInt()
+    val value = random.nextInt
     if (value > 0) {
       value
     } else {
@@ -120,17 +114,5 @@ case class Random() {
       positiveLong()
     }
   }
-
-}
-
-object Random {
-
-  private val Ambiguous = "B8G6I1l0OoQDS5Z2".split("")
-  private val Numbers = "0123456789"
-  private val Lower = "abcdefghijklmnopqrstuvwxyz"
-  private val LowerAndUpper = Lower + Lower.toUpperCase
-  private val LowerAndUpperAndNumbers = LowerAndUpper + Numbers
-  private val NonAmbiguousLowerAndUpper = LowerAndUpper.split("").filter(!Ambiguous.contains(_)).mkString("")
-  private val NonAmbiguousLowerAndUpperAndNumbers = NonAmbiguousLowerAndUpper + "3479"
 
 }
---
+++ app/io/flow/play/util/RefreshingReference.scala
@@ -0,0 +1,107 @@
+package io.flow.play.util
+
+import java.util.concurrent.atomic.AtomicReference
+
+import akka.actor.Scheduler
+import play.api.Logger
+
+import scala.concurrent.ExecutionContext
+import scala.concurrent.duration.FiniteDuration
+import scala.util.{Failure, Success, Try}
+
+/**
+  * Maintains a reference that gets refreshed every `reloadInterval`
+  *
+  * The data is refreshed every `reloadInterval` period calling the `retrieve` function up to `maxAtttempts` times
+  * Upon successful completion, the data is refreshed with the retrieved data, otherwise the cache is not refreshed.
+  *
+  * The class will not initialize if the retrieval function fails the first `maxAttempts` times to avoid querying a
+  * cache that has never been initialized.
+  */
+trait RefreshingReference[T] {
+
+  /**
+    * The scheduler to use to schedule the [[retrieve]] function every [[reloadInterval]] period.
+    */
+  def scheduler: Scheduler
+
+  /**
+    * The context to use to execute the [[retrieve]] function every [[reloadInterval]] period.
+    */
+  def retrieveExecutionContext: ExecutionContext
+
+  /**
+    * Amount
+    */
+  def reloadInterval: FiniteDuration
+
+  /**
+    * Function retuning the data to cache.
+    * It is called every [[reloadInterval]] period.
+    * If successful, the cache is refreshed with the latest data, otherwise the cache is not refreshed.
+    */
+  def retrieve: T
+
+  /**
+    * Maximum number of attempts to retrieve the data.
+    * If the [[retrieve]] function fails [[maxAttempts]] times in a row, the cache will not refresh and will retry
+    * to retrieve data after [[reloadInterval]].
+    *
+    * Default: 3
+    */
+  def maxAttempts: Int = 3
+
+  def get: T = cache.get()
+
+  // load blocking and fail if the retrieval is not successful after maxAttempts
+  private[this] val cache: AtomicReference[T] = {
+    val retrieved = doLoadRetry(1, maxAttempts) match {
+      case Success(data) => data
+      case Failure(ex) =>
+        Logger.warn(s"Failed initializing cache after $maxAttempts attempt${if (maxAttempts > 1) "s" else ""}.", ex)
+        throw ex
+    }
+    new AtomicReference(retrieved)
+  }
+
+  // schedule subsequent reloads
+  scheduler.schedule(reloadInterval, reloadInterval) {
+    doLoadRetry(1, maxAttempts) match {
+      case Success(data) => cache.set(data)
+      case Failure(ex) =>
+        Logger.warn(s"Failed refreshing cache after $maxAttempts attempt${if (maxAttempts > 1) "s" else ""}. " +
+          s"Will try again in $reloadInterval", ex)
+    }
+  }(retrieveExecutionContext)
+
+  private def doLoadRetry(attempts: Int, maxAttempts: Int): Try[T] =
+    Try(retrieve)
+      .recoverWith { case ex if attempts < maxAttempts =>
+        Logger.warn(s"Failed refreshing cache at attempt $attempts/$maxAttempts. Trying again...", ex)
+        doLoadRetry(attempts + 1, maxAttempts)
+      }
+
+}
+
+object RefreshingReference {
+
+  /**
+    * Helper function to create a new [[RefreshingReference]]
+    */
+  def apply[T](scheduler: Scheduler, retrieveExecutionContext: ExecutionContext, reloadInterval: FiniteDuration,
+               retrieve: () => T, maxAttempts: Int = 3): RefreshingReference[T] = {
+    val schedulerOuter = scheduler
+    val retrieveExecutionContextOuter = retrieveExecutionContext
+    val reloadIntervalOuter = reloadInterval
+    val retrieveOuter = retrieve
+    val maxAttemptsOuter = maxAttempts
+    new RefreshingReference[T]() {
+      override def scheduler: Scheduler = schedulerOuter
+      override def retrieveExecutionContext: ExecutionContext = retrieveExecutionContextOuter
+      override def reloadInterval: FiniteDuration = reloadIntervalOuter
+      override def retrieve: T = retrieveOuter()
+      override def maxAttempts: Int = maxAttemptsOuter
+    }
+  }
+
+}

c22197f8ac633e63aed528ce780fa72ab468c9d2
Ben Iofel <ben@flow.io>
2018-01-26 17:45:15 -0500

    play2.6 logging updates

--- app/io/flow/play/util/LoggingUtil.scala
+++ app/io/flow/play/util/LoggingUtil.scala
@@ -1,17 +1,14 @@
 package io.flow.play.util
 
-import net.logstash.logback.marker.LogstashMarker
 import net.logstash.logback.marker.Markers.appendEntries
-import play.api.Logger
+import play.api.{Logger, MarkerContext}
 
 import scala.collection.JavaConverters._
 
 trait Logging {
-  protected lazy val logger = Logger(getClass).logger
-}
+  protected lazy val logger = Logger(getClass)
 
-object LoggingUtil {
-  def append(fields: (String, Any)*): LogstashMarker = {
-    appendEntries(fields.toMap.asJava)
+  @inline def logWith(fields: (String, Any)*): MarkerContext = {
+    MarkerContext(appendEntries(fields.toMap.asJava))
   }
 }

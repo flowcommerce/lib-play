bfcd37e60b6b7b790f9b18d3178c57b9558c89ff
Jean Logeart <jean.logeart@gmail.com>
2018-03-15 11:16:27 -0400

    Remove mockable api

--- app/io/flow/play/controllers/FlowController.scala
+++ app/io/flow/play/controllers/FlowController.scala
@@ -4,11 +4,11 @@ import javax.inject.Inject
 
 import com.google.inject.ImplementedBy
 import io.flow.common.v0.models.UserReference
-import io.flow.play.util.MockableApiUtil._
 import io.flow.play.util.{AuthData, AuthHeaders, Config, OrgAuthData}
 import play.api.mvc._
 
 import scala.concurrent.{ExecutionContext, Future}
+import scala.language.higherKinds
 
 /**
   USAGE:
@@ -31,13 +31,6 @@ trait FlowController extends BaseController with FlowControllerHelpers {
   def SessionOrg: SessionOrgActionBuilder = flowControllerComponents.sessionOrgActionBuilder
   def IdentifiedCookie: IdentifiedCookieActionBuilder = flowControllerComponents.identifiedCookieActionBuilder
 
-  def MockableAnonymous: MockableAnonymousActionBuilder = flowControllerComponents.mockableAnonymousActionBuilder
-  def MockableIdentified: MockableIdentifiedActionBuilder = flowControllerComponents.mockableIdentifiedActionBuilder
-  def MockableSession: MockableSessionActionBuilder = flowControllerComponents.mockableSessionActionBuilder
-  def MockableOrg: MockableOrgActionBuilder = flowControllerComponents.mockableOrgActionBuilder
-  def MockableIdentifiedOrg: MockableIdentifiedOrgActionBuilder = flowControllerComponents.mockableIdentifiedOrgActionBuilder
-  def MockableSessionOrg: MockableSessionOrgActionBuilder = flowControllerComponents.mockableSessionOrgActionBuilder
-  def MockableIdentifiedCookie: MockableIdentifiedCookieActionBuilder = flowControllerComponents.mockableIdentifiedCookieActionBuilder
 }
 
 @ImplementedBy(classOf[FlowDefaultControllerComponents])
@@ -49,37 +42,21 @@ trait FlowControllerComponents {
   def identifiedOrgActionBuilder: IdentifiedOrgActionBuilder
   def sessionOrgActionBuilder: SessionOrgActionBuilder
   def identifiedCookieActionBuilder: IdentifiedCookieActionBuilder
-
-  def mockableAnonymousActionBuilder: MockableAnonymousActionBuilder
-  def mockableIdentifiedActionBuilder: MockableIdentifiedActionBuilder
-  def mockableSessionActionBuilder: MockableSessionActionBuilder
-  def mockableOrgActionBuilder: MockableOrgActionBuilder
-  def mockableIdentifiedOrgActionBuilder: MockableIdentifiedOrgActionBuilder
-  def mockableSessionOrgActionBuilder: MockableSessionOrgActionBuilder
-  def mockableIdentifiedCookieActionBuilder: MockableIdentifiedCookieActionBuilder
 }
 
-case class FlowDefaultControllerComponents @Inject()(
+case class FlowDefaultControllerComponents @Inject() (
   anonymousActionBuilder: AnonymousActionBuilder,
   identifiedActionBuilder: IdentifiedActionBuilder,
   sessionActionBuilder: SessionActionBuilder,
   orgActionBuilder: OrgActionBuilder,
   identifiedOrgActionBuilder: IdentifiedOrgActionBuilder,
   sessionOrgActionBuilder: SessionOrgActionBuilder,
-  identifiedCookieActionBuilder: IdentifiedCookieActionBuilder,
-
-  mockableAnonymousActionBuilder: MockableAnonymousActionBuilder,
-  mockableIdentifiedActionBuilder: MockableIdentifiedActionBuilder,
-  mockableSessionActionBuilder: MockableSessionActionBuilder,
-  mockableOrgActionBuilder: MockableOrgActionBuilder,
-  mockableIdentifiedOrgActionBuilder: MockableIdentifiedOrgActionBuilder,
-  mockableSessionOrgActionBuilder: MockableSessionOrgActionBuilder,
-  mockableIdentifiedCookieActionBuilder: MockableIdentifiedCookieActionBuilder
-
+  identifiedCookieActionBuilder: IdentifiedCookieActionBuilder
 ) extends FlowControllerComponents
 
+
 // Anonymous
-class AnonymousActionBuilder @Inject()(val parser: BodyParsers.Default, val config: Config)(implicit val executionContext: ExecutionContext)
+class AnonymousActionBuilder @Inject() (val parser: BodyParsers.Default, val config: Config)(implicit val executionContext: ExecutionContext)
   extends ActionBuilder[AnonymousRequest, AnyContent] with FlowActionInvokeBlockHelper {
 
   def invokeBlock[A](request: Request[A], block: (AnonymousRequest[A]) => Future[Result]): Future[Result] = {
@@ -92,7 +69,7 @@ class AnonymousActionBuilder @Inject()(val parser: BodyParsers.Default, val conf
 }
 
 // Identified
-class IdentifiedActionBuilder @Inject()(val parser: BodyParsers.Default, val config: Config)(implicit val executionContext: ExecutionContext)
+class IdentifiedActionBuilder @Inject() (val parser: BodyParsers.Default, val config: Config)(implicit val executionContext: ExecutionContext)
   extends ActionBuilder[IdentifiedRequest, AnyContent] with FlowActionInvokeBlockHelper {
 
   def invokeBlock[A](request: Request[A], block: (IdentifiedRequest[A]) => Future[Result]): Future[Result] =
@@ -103,7 +80,7 @@ class IdentifiedActionBuilder @Inject()(val parser: BodyParsers.Default, val con
 }
 
 // Session
-class SessionActionBuilder @Inject()(val parser: BodyParsers.Default, val config: Config)(implicit val executionContext: ExecutionContext)
+class SessionActionBuilder @Inject() (val parser: BodyParsers.Default, val config: Config)(implicit val executionContext: ExecutionContext)
   extends ActionBuilder[SessionRequest, AnyContent] with FlowActionInvokeBlockHelper {
 
   def invokeBlock[A](request: Request[A], block: (SessionRequest[A]) => Future[Result]): Future[Result] =
@@ -114,7 +91,7 @@ class SessionActionBuilder @Inject()(val parser: BodyParsers.Default, val config
 }
 
 // Org
-class OrgActionBuilder @Inject()(val parser: BodyParsers.Default, val config: Config)(implicit val executionContext: ExecutionContext)
+class OrgActionBuilder @Inject() (val parser: BodyParsers.Default, val config: Config)(implicit val executionContext: ExecutionContext)
   extends ActionBuilder[OrgRequest, AnyContent] with FlowActionInvokeBlockHelper {
 
   def invokeBlock[A](request: Request[A], block: (OrgRequest[A]) => Future[Result]): Future[Result] =
@@ -125,7 +102,7 @@ class OrgActionBuilder @Inject()(val parser: BodyParsers.Default, val config: Co
 }
 
 // IdentifiedOrg
-class IdentifiedOrgActionBuilder @Inject()(val parser: BodyParsers.Default, val config: Config)(implicit val executionContext: ExecutionContext)
+class IdentifiedOrgActionBuilder @Inject() (val parser: BodyParsers.Default, val config: Config)(implicit val executionContext: ExecutionContext)
   extends ActionBuilder[IdentifiedOrgRequest, AnyContent] with FlowActionInvokeBlockHelper {
 
   def invokeBlock[A](request: Request[A], block: (IdentifiedOrgRequest[A]) => Future[Result]): Future[Result] =
@@ -136,18 +113,18 @@ class IdentifiedOrgActionBuilder @Inject()(val parser: BodyParsers.Default, val 
 }
 
 // SessionOrg
-class SessionOrgActionBuilder @Inject()(val parser: BodyParsers.Default, val config: Config)(implicit val executionContext: ExecutionContext)
+class SessionOrgActionBuilder @Inject() (val parser: BodyParsers.Default, val config: Config)(implicit val executionContext: ExecutionContext)
   extends ActionBuilder[SessionOrgRequest, AnyContent] with FlowActionInvokeBlockHelper {
 
   def invokeBlock[A](request: Request[A], block: (SessionOrgRequest[A]) => Future[Result]): Future[Result] =
     auth(request.headers)(OrgAuthData.Session.fromMap) match {
-      case None => Future.successful(unauthorized(request))
+      case None => Future.successful (unauthorized(request))
       case Some(ad) => block(new SessionOrgRequest(ad, request))
     }
 }
 
 // IdentifiedCookie
-class IdentifiedCookieActionBuilder @Inject()(val parser: BodyParsers.Default, val config: Config)(implicit val executionContext: ExecutionContext)
+class IdentifiedCookieActionBuilder @Inject() (val parser: BodyParsers.Default, val config: Config)(implicit val executionContext: ExecutionContext)
   extends ActionBuilder[IdentifiedRequest, AnyContent] with FlowActionInvokeBlockHelper {
 
   def invokeBlock[A](request: Request[A], block: (IdentifiedRequest[A]) => Future[Result]): Future[Result] =
@@ -164,105 +141,7 @@ object IdentifiedCookie {
   val UserKey = "user_id"
 
   implicit class ResultWithUser(val result: Result) extends AnyVal {
-    def withIdentifiedCookieUser(user: UserReference): Result = result.withSession(UserKey -> user.id.toString)
+    def withIdentifiedCookieUser(user: UserReference) = result.withSession(UserKey -> user.id.toString)
   }
 
-}
-
-
-
-
-// Mockable Anonymous
-class MockableAnonymousActionBuilder @Inject()(val parser: BodyParsers.Default, val config: Config)(implicit val executionContext: ExecutionContext)
-  extends ActionBuilder[AnonymousRequest, AnyContent] with FlowActionInvokeBlockHelper {
-
-  def invokeBlock[A](request: Request[A], block: (AnonymousRequest[A]) => Future[Result]): Future[Result] = {
-    withMockableApis(request) {
-      val ad = auth(request.headers)(AuthData.Anonymous.fromMap).getOrElse {
-        // Create an empty header here so at least requestId tracking can start
-        AuthData.Anonymous.Empty
-      }
-      block(new AnonymousRequest(ad, request))
-    }
-  }
-}
-
-// Mockable Identified
-class MockableIdentifiedActionBuilder @Inject()(val parser: BodyParsers.Default, val config: Config)(implicit val executionContext: ExecutionContext)
-  extends ActionBuilder[IdentifiedRequest, AnyContent] with FlowActionInvokeBlockHelper {
-
-  def invokeBlock[A](request: Request[A], block: (IdentifiedRequest[A]) => Future[Result]): Future[Result] =
-    withMockableApis(request) {
-      auth(request.headers)(AuthData.Identified.fromMap) match {
-        case None => Future.successful(unauthorized(request))
-        case Some(ad) => block(new IdentifiedRequest(ad, request))
-      }
-    }
-}
-
-// Mockable Session
-class MockableSessionActionBuilder @Inject()(val parser: BodyParsers.Default, val config: Config)(implicit val executionContext: ExecutionContext)
-  extends ActionBuilder[SessionRequest, AnyContent] with FlowActionInvokeBlockHelper {
-
-  def invokeBlock[A](request: Request[A], block: (SessionRequest[A]) => Future[Result]): Future[Result] =
-    withMockableApis(request) {
-      auth(request.headers)(AuthData.Session.fromMap) match {
-        case None => Future.successful(unauthorized(request))
-        case Some(ad) => block(new SessionRequest(ad, request))
-      }
-    }
-}
-
-// Mockable Org
-class MockableOrgActionBuilder @Inject()(val parser: BodyParsers.Default, val config: Config)(implicit val executionContext: ExecutionContext)
-  extends ActionBuilder[OrgRequest, AnyContent] with FlowActionInvokeBlockHelper {
-
-  def invokeBlock[A](request: Request[A], block: (OrgRequest[A]) => Future[Result]): Future[Result] =
-    withMockableApis(request) {
-      auth(request.headers)(OrgAuthData.Org.fromMap) match {
-        case None => Future.successful(unauthorized(request))
-        case Some(ad) => block(new OrgRequest(ad, request))
-      }
-    }
-}
-
-// Mockable IdentifiedOrg
-class MockableIdentifiedOrgActionBuilder @Inject()(val parser: BodyParsers.Default, val config: Config)(implicit val executionContext: ExecutionContext)
-  extends ActionBuilder[IdentifiedOrgRequest, AnyContent] with FlowActionInvokeBlockHelper {
-
-  def invokeBlock[A](request: Request[A], block: (IdentifiedOrgRequest[A]) => Future[Result]): Future[Result] =
-    withMockableApis(request) {
-      auth(request.headers)(OrgAuthData.Identified.fromMap) match {
-        case None => Future.successful(unauthorized(request))
-        case Some(ad) => block(new IdentifiedOrgRequest(ad, request))
-      }
-    }
-}
-
-// Mockable SessionOrg
-class MockableSessionOrgActionBuilder @Inject()(val parser: BodyParsers.Default, val config: Config)(implicit val executionContext: ExecutionContext)
-  extends ActionBuilder[SessionOrgRequest, AnyContent] with FlowActionInvokeBlockHelper {
-
-  def invokeBlock[A](request: Request[A], block: (SessionOrgRequest[A]) => Future[Result]): Future[Result] =
-    withMockableApis(request) {
-      auth(request.headers)(OrgAuthData.Session.fromMap) match {
-        case None => Future.successful(unauthorized(request))
-        case Some(ad) => block(new SessionOrgRequest(ad, request))
-      }
-    }
-}
-
-// Mockable IdentifiedCookie
-class MockableIdentifiedCookieActionBuilder @Inject()(val parser: BodyParsers.Default, val config: Config)(implicit val executionContext: ExecutionContext)
-  extends ActionBuilder[IdentifiedRequest, AnyContent] with FlowActionInvokeBlockHelper {
-
-  def invokeBlock[A](request: Request[A], block: (IdentifiedRequest[A]) => Future[Result]): Future[Result] =
-    withMockableApis(request) {
-      request.session.get(IdentifiedCookie.UserKey) match {
-        case None => Future.successful(unauthorized(request))
-        case Some(userId) =>
-          val auth = AuthHeaders.user(UserReference(id = userId))
-          block(new IdentifiedRequest(auth, request))
-      }
-    }
 }

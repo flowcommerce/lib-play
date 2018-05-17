13e77f83fe7215afa5b34eefd0f669c803b2e78b
Jean Logeart <jean.logeart@gmail.com>
2018-05-17 11:00:33 -0400

    Log when secrets are updated

--- app/io/flow/play/jwt/JwtSecretsRetrieverService.scala
+++ app/io/flow/play/jwt/JwtSecretsRetrieverService.scala
@@ -3,10 +3,12 @@ package io.flow.play.jwt
 import java.util.concurrent.atomic.AtomicReference
 
 import akka.actor.ActorSystem
-import io.flow.secret.internal.v0.models.{SecretConfig, Secret => JwtSecret}
+import io.flow.secret.internal.v0.models.{RedactedSecret, RedactedSecretConfig, SecretConfig, Secret => JwtSecret}
+import io.flow.secret.internal.v0.models.json._
 import javax.inject.{Inject, Named, Singleton}
 import org.joda.time.DateTime
 import play.api.Logger
+import play.api.libs.json.Json
 
 import scala.concurrent.duration._
 import scala.concurrent.{Await, ExecutionContext, Future}
@@ -37,7 +39,8 @@ class RefreshingJwtSecretsRetrieverService @Inject() (
         Logger.error(s"[JwtSecretsError] Error when initializing JWT secrets after $MaxAttempts attempts. Failed to initialize.", ex)
       case _ =>
     }
-    new AtomicReference[JwtSecrets](Await.result(secretsFuture, 30.seconds))
+    val (_, transformedSecrets) = Await.result(secretsFuture, 30.seconds)
+    new AtomicReference[JwtSecrets](transformedSecrets)
   }
 
   system.scheduler.schedule(reloadInterval, reloadInterval)(getAndUpdate())
@@ -52,8 +55,8 @@ class RefreshingJwtSecretsRetrieverService @Inject() (
       .getOrElse(Failure(new IllegalStateException(s"Preferred id [${jwtSecrets.preferredSecretId}] could not be found in the list of provided secrets")))
   }
 
-  private def getAndTransformRetries(attempts: Int): Future[JwtSecrets] = {
-    jwtSecretsDao.get.flatMap(s => Future.fromTry(transformSecrets(s))).recoverWith {
+  private def getAndTransformRetries(attempts: Int): Future[(SecretConfig, JwtSecrets)] = {
+    jwtSecretsDao.get.flatMap(s => Future.fromTry(transformSecrets(s).map(t => (s, t)))).recoverWith {
       case t: Throwable if attempts < MaxAttempts =>
         Logger.warn(s"[JwtSecretsWarn] Error when retrieving JWT secrets ($attempts/$MaxAttempts). Retrying...", t)
         getAndTransformRetries(attempts + 1)
@@ -62,9 +65,21 @@ class RefreshingJwtSecretsRetrieverService @Inject() (
 
   private def getAndUpdate(): Unit = {
     getAndTransformRetries(1).onComplete {
-      case Success(res) => secrets.set(res)
+      case Success((newConfig, newTransformed)) =>
+        setAndLogNew(newConfig, newTransformed)
       case Failure(ex) =>
         Logger.error(s"[JwtSecretsError] Error when retrieving JWT secrets after $MaxAttempts attempts. Jwt secrets not updating.", ex)
+    }
+  }
+
+  private def setAndLogNew(newConfig: SecretConfig, newTransformed: JwtSecrets): Unit = {
+    val old = secrets.getAndSet(newTransformed)
+    if (old != newTransformed) {
+      val redactedNew = RedactedSecretConfig(
+        secrets = newConfig.secrets.map(s => RedactedSecret(id = s.id, createdAt = s.createdAt)),
+        preferredSecretId = newConfig.preferredSecretId
+      )
+      Logger.info(s"[RefreshingJwtSecretsRetrieverService] updated secrets to ${Json.toJson(redactedNew).toString()}")
     }
   }
 

2739219ba8cb5736e627ec3e98f056cfb0ea7975
Jean Logeart <jean.logeart@gmail.com>
2018-05-18 08:02:43 -0400

    Parse bucket and key from s3 uri

--- app/io/flow/play/jwt/JwtSecretsDao.scala
+++ app/io/flow/play/jwt/JwtSecretsDao.scala
@@ -1,14 +1,17 @@
 package io.flow.play.jwt
 
+import java.net.URI
+
 import akka.stream.Materializer
 import akka.stream.alpakka.s3.scaladsl.S3Client
 import io.flow.play.util.Config
-import io.flow.secret.internal.v0.models.json._
 import io.flow.secret.internal.v0.models.SecretConfig
+import io.flow.secret.internal.v0.models.json._
 import javax.inject.{Inject, Singleton}
 import play.api.libs.json.Json
 
 import scala.concurrent.{ExecutionContext, Future}
+import scala.util.{Failure, Success, Try}
 
 trait JwtSecretsDao {
 
@@ -22,12 +25,26 @@ class S3JwtSecretsDao @Inject() (
   s3Client: S3Client
 )(implicit ec: ExecutionContext, mat: Materializer) extends JwtSecretsDao {
 
-  private val s3Bucket = config.requiredString("jwt.secrets.s3.bucket")
-  private val s3Key = config.requiredString("jwt.secrets.s3.key")
+  private val s3Uri = config.requiredString("jwt.secrets.s3.uri")
+  private val (s3Bucket, s3Key) = S3JwtSecretsDao.getBucketAndKey(s3Uri).get
 
   override def get: Future[SecretConfig] = {
+
     val (source, _) = s3Client.download(s3Bucket, s3Key)
     source.runReduce(_ ++ _).map(s => Json.parse(s.utf8String).as[SecretConfig])
   }
+
+}
+
+object S3JwtSecretsDao {
+
+  def getBucketAndKey(uri: String): Try[(String, String)] =
+    Try(new URI(uri))
+    .flatMap { uriUri =>
+      if (uriUri.getScheme.toLowerCase == "s3")
+        Success((uriUri.getHost, uriUri.getPath.substring(1)))
+      else
+        Failure(new IllegalArgumentException("s3 URI must have s3 scheme"))
+    }
 
 }
--- test/io/flow/play/jwt/JwtSecretsDaoSpec.scala
+++ test/io/flow/play/jwt/JwtSecretsDaoSpec.scala
@@ -1,5 +1,7 @@
 package io.flow.play.jwt
 
+import java.net.URISyntaxException
+
 import akka.stream.Materializer
 import io.flow.play.aws.S3Util
 import io.flow.secret.internal.v0.models.json._
@@ -7,7 +9,7 @@ import io.flow.secret.internal.v0.models.{Secret, SecretConfig}
 import org.joda.time.DateTime
 import org.scalatest.concurrent.PatienceConfiguration.Timeout
 import org.scalatest.concurrent.ScalaFutures
-import org.scalatest.{BeforeAndAfterAll, BeforeAndAfterEach}
+import org.scalatest.{BeforeAndAfterAll, BeforeAndAfterEach, TryValues}
 import org.scalatestplus.play.PlaySpec
 import org.scalatestplus.play.guice.GuiceOneAppPerSuite
 import play.api.Application
@@ -19,13 +21,13 @@ import scala.concurrent.ExecutionContext
 import scala.concurrent.duration._
 
 class JwtSecretsDaoSpec extends PlaySpec with GuiceOneAppPerSuite with BeforeAndAfterAll with BeforeAndAfterEach
-  with ScalaFutures with S3Util {
+  with ScalaFutures with S3Util with TryValues {
 
-  private val s3bucket = "bucket"
-  private val s3key = "key"
+  private val s3Uri = "s3://bucket/key"
+  private val (s3Bucket, s3Key) = S3JwtSecretsDao.getBucketAndKey(s3Uri).get
 
   override def fakeApplication(): Application = {
-    new GuiceApplicationBuilder().configure("jwt.secrets.s3.bucket" -> s3bucket, "jwt.secrets.s3.key" -> s3key).build()
+    new GuiceApplicationBuilder().configure("jwt.secrets.s3.uri" -> s3Uri).build()
   }
 
   private implicit val ec: ExecutionContext = app.actorSystem.dispatcher
@@ -35,29 +37,45 @@ class JwtSecretsDaoSpec extends PlaySpec with GuiceOneAppPerSuite with BeforeAnd
 
   override def beforeEach(): Unit = {
     super.beforeEach()
-    s3MockClient.listBuckets().asScala.map(_.getName).filter(_ == s3bucket).foreach(s3MockClient.deleteBucket)
-    s3MockClient.createBucket(s3bucket)
+    s3MockClient.listBuckets().asScala.map(_.getName).filter(_ == s3Bucket).foreach(s3MockClient.deleteBucket)
+    s3MockClient.createBucket(s3Bucket)
   }
 
   "S3JwtSecretsDao" should {
+
+    "extract bucket and key from uri" in {
+      val (bucket, key) = S3JwtSecretsDao.getBucketAndKey("s3://bucket/path/to/file.txt").success.value
+      bucket mustBe "bucket"
+      key mustBe "path/to/file.txt"
+    }
+
+    "fail to extract bucket and key if scheme is not s3" in {
+      val failure = S3JwtSecretsDao.getBucketAndKey("http://bucket.s3.amazon.com/path/to/file.txt").failure
+      failure.exception mustBe a[IllegalArgumentException]
+    }
+
+    "fail to extract bucket and key if uri is malformed" in {
+      val failure = S3JwtSecretsDao.getBucketAndKey("not a valid uri").failure
+      failure.exception mustBe a[URISyntaxException]
+    }
 
     "get secrets from s3" in {
       val secretsConfig = SecretConfig(
         secrets = Seq(Secret("id1", "key1", DateTime.now), Secret("id2", "key2", DateTime.now)),
         preferredSecretId = "id2"
       )
-      s3MockClient.putObject(s3bucket, s3key, Json.toJson(secretsConfig).toString())
+      s3MockClient.putObject(s3Bucket, s3Key, Json.toJson(secretsConfig).toString())
 
       dao.get.futureValue(Timeout(2.seconds)) mustBe secretsConfig
     }
 
     "fail if file cannot be found" in {
-      s3MockClient.deleteBucket(s3bucket)
+      s3MockClient.deleteBucket(s3Bucket)
       dao.get.failed.futureValue mustBe a[akka.stream.alpakka.s3.S3Exception]
     }
 
     "fail if file content cannot be parsed" in {
-      s3MockClient.putObject(s3bucket, s3key, "not even json")
+      s3MockClient.putObject(s3Bucket, s3Key, "not even json")
       dao.get.failed.futureValue mustBe a[com.fasterxml.jackson.core.JsonParseException]
     }
 

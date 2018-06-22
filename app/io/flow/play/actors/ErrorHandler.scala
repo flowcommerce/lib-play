9550a20210b03aeb600940c39d578827207ad287
Jakub Kozłowski <kubukoz@gmail.com>
2018-06-22 22:28:32 +0200

    Add logErrors to ErrorHandler

--- app/io/flow/play/actors/ErrorHandler.scala
+++ app/io/flow/play/actors/ErrorHandler.scala
@@ -1,18 +1,38 @@
 package io.flow.play.actors
 
+import akka.actor.Actor.Receive
 import play.api.Logger
 
 /**
   * Common utilities for handling and logging errors in actors
   */
 trait ErrorHandler {
+  /**
+    * Wraps an [[akka.actor.Actor.Receive]] with error handling that will catch any throwable and log it.
+    *
+    * Example usage:
+    *
+    * class MainActor extends Actor with ErrorHandler {
+    *   def receive: Receive = logErrors {
+    *     case MyMessage => throw new NullPointerException
+    *   }
+    * }
+    *
+    * Equivalent to wrapping every message handler with `withErrorHandler(msg)`.
+    **/
+  def logErrors(handler: Receive): Receive = {
+    case msg if handler.isDefinedAt(msg) =>
+      withErrorHandler(msg) {
+        handler(msg)
+      }
+  }
 
   /**
     * Wraps a block with error handling that will catch any throwable and log it.
     *
     * Example usage:
     * 
-    * class MainActor(name: String) extends Actor with ActorLogging with Util {
+    * class MainActor(name: String) extends Actor with ActorLogging with ErrorHandler {
     * 
     * def receive = akka.event.LoggingReceive {
     *   
@@ -43,17 +63,17 @@ trait ErrorHandler {
     * catch any throwable and log it.
     *
     * Example usage:
-    * 
-    * class MainActor(name: String) extends Actor with ActorLogging with Util {
-    * 
+    *
+    * class MainActor(name: String) extends Actor with ActorLogging with ErrorHandler {
+    *
     * def receive = akka.event.LoggingReceive {
-    *   
+    *
     *   case m @ MainActor.Messages.ExampleMessage => withVerboseErrorHandler(m) {
     *     ...
     *   }
     *
     *   case m: Any => logUnhandledMessage(m)
-    * 
+    *
     * }
     */
   def withVerboseErrorHandler[T](

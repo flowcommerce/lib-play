6161738ebcb1f4bbdddd994b0cb396683bd89eee
Jakub Kozłowski <kubukoz@gmail.com>
2018-07-31 00:05:59 +0200

    Un-deprecate type alias

--- app/io/flow/play/util/package.scala
+++ app/io/flow/play/util/package.scala
@@ -1,9 +1,9 @@
 package io.flow.play
 
 package object util {
-  @deprecated("Deprecated in favour of lib-util (io.flow.util.*)", "0.4.78")
   type Config = io.flow.util.Config
+  type ChainedConfig = io.flow.util.ChainedConfig
+
   val EnvironmentConfig: Config = io.flow.util.EnvironmentConfig
   val PropertyConfig: Config = io.flow.util.PropertyConfig
-  type ChainedConfig = io.flow.util.ChainedConfig
 }

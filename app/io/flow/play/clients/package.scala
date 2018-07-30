702d6634002a5ca594c5cf0854aceab168b82bb5
Jakub Kozłowski <kubukoz@gmail.com>
2018-07-31 00:09:25 +0200

    Remove deprecations

--- app/io/flow/play/clients/package.scala
+++ app/io/flow/play/clients/package.scala
@@ -1,14 +1,9 @@
 package io.flow.play
-
-import io.flow.util.clients.{Registry => NewRegistry, ProductionRegistry => NewProductionRegistry, MockRegistry => NewMockRegistry}
+import io.flow.util.{clients => libutil}
 
 package object clients {
-  @deprecated("Deprecated in favour of lib-util (io.flow.util.*)", "0.4.78")
-  type Registry = NewRegistry
 
-  @deprecated("Deprecated in favour of lib-util (io.flow.util.*)", "0.4.78")
-  type ProductionRegistry = NewProductionRegistry
-
-  @deprecated("Deprecated in favour of lib-util (io.flow.util.*)", "0.4.78")
-  type MockRegistry = NewMockRegistry
+  type Registry = libutil.Registry
+  type ProductionRegistry = libutil.ProductionRegistry
+  type MockRegistry = libutil.MockRegistry
 }

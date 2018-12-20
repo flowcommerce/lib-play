7fac83e948683a139b46b517cbc8f0f420c5c99f
Gregor Heine <gheine@users.noreply.github.com>
2018-12-20 11:18:36 +0000

    Upgrade lib-util, lib-log, lib-akka versions (#262)
    * Upgrade lib-util, lib-log, lib-akka versions
    
    Pass RollbarLogger to underlying SafeReceive
    
    * lib-util once more

--- app/io/flow/play/actors/proxy/SQSSenderProxyActor.scala
+++ app/io/flow/play/actors/proxy/SQSSenderProxyActor.scala
@@ -4,11 +4,13 @@ import akka.actor.{Actor, ActorLogging}
 import com.amazonaws.services.sqs.AmazonSQSAsync
 import com.amazonaws.services.sqs.model.SendMessageRequest
 import io.flow.akka.SafeReceive
+import io.flow.log.RollbarLogger
 import play.api.libs.json.Json
 
-class SQSSenderProxyActor(receiverActorName: String, sqs: AmazonSQSAsync, serviceName: String, serde: ProxySerde) extends Actor with ActorLogging {
+class SQSSenderProxyActor(receiverActorName: String, sqs: AmazonSQSAsync, serviceName: String, serde: ProxySerde, rollbar: RollbarLogger) extends Actor with ActorLogging {
 
   private val QueueUrl = SQSProxyActor.generateQueueUrl(receiverActorName, serviceName, sqs)
+  private implicit val configuredRollbar: RollbarLogger = rollbar.fingerprint("SQSSenderProxyActor").withKeyValue("class", getClass.getName)
 
   override def receive: Receive = SafeReceive {
     case msg => sqs.sendMessage(new SendMessageRequest()

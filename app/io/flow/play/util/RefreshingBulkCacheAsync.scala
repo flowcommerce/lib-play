67229c785342066f06e04c206ed71499172067db
Jean Logeart <jean.logeart@gmail.com>
2019-05-14 13:19:12 -0400

    Add RefreshingBulkCacheAsync

---
+++ app/io/flow/play/util/RefreshingBulkCacheAsync.scala
@@ -0,0 +1,63 @@
+package io.flow.play.util
+
+import akka.actor.{ActorSystem, Scheduler}
+import io.flow.log.RollbarLogger
+
+import scala.concurrent.duration.{FiniteDuration, _}
+import scala.concurrent.{ExecutionContext, Future}
+
+trait RefreshingBulkCacheAsync[K, V] extends RefreshingReferenceAsync[Map[K, V]] {
+
+  def getKey(key: K): Option[V] = super.get.get(key)
+
+}
+
+object RefreshingBulkCacheAsync {
+
+  /**
+    * Helper function to create a new [[RefreshingReference]]
+    */
+  def apply[K, V](
+    retrieve: () => Future[Map[K, V]],
+    scheduler: Scheduler,
+    retrieveExecutionContext: ExecutionContext,
+    rollbarLogger: RollbarLogger,
+    reloadInterval: FiniteDuration = 1.minute,
+    maxAttempts: Int = 3,
+  ): RefreshingBulkCacheAsync[K, V] = {
+    val schedulerOuter = scheduler
+    val retrieveExecutionContextOuter = retrieveExecutionContext
+    val reloadIntervalOuter = reloadInterval
+    val retrieveOuter = retrieve
+    val maxAttemptsOuter = maxAttempts
+    new RefreshingBulkCacheAsync[K, V]() {
+      override def logger: RollbarLogger = rollbarLogger
+      override def scheduler: Scheduler = schedulerOuter
+      override def retrieveExecutionContext: ExecutionContext = retrieveExecutionContextOuter
+      override def reloadInterval: FiniteDuration = reloadIntervalOuter
+      override def retrieve: Future[Map[K, V]] = retrieveOuter()
+      override def maxAttempts: Int = maxAttemptsOuter
+    }
+  }
+
+  /**
+    * Helper function to create a new [[RefreshingReference]]
+    *
+    * Uses the system's scheduler and default dispatcher
+    */
+  def fromActorSystem[K, V](
+    retrieve: () => Future[Map[K, V]],
+    system: ActorSystem,
+    rollbarLogger: RollbarLogger,
+    reloadInterval: FiniteDuration = 1.minute,
+    maxAttempts: Int = 3
+  ): RefreshingBulkCacheAsync[K, V] = apply(
+    retrieve = retrieve,
+    scheduler = system.scheduler,
+    retrieveExecutionContext = system.dispatcher,
+    reloadInterval = reloadInterval,
+    rollbarLogger = rollbarLogger,
+    maxAttempts = maxAttempts
+  )
+
+}
---
+++ test/io/flow/play/util/RefreshingBulkCacheAsyncSpec.scala
@@ -0,0 +1,33 @@
+package io.flow.play.util
+
+import io.flow.log.RollbarProvider
+import org.scalatest.{Matchers, OptionValues, WordSpec}
+import org.scalatestplus.play.guice.GuiceOneAppPerSuite
+
+import scala.concurrent.Future
+import scala.concurrent.duration.{FiniteDuration, _}
+
+class RefreshingBulkCacheAsyncSpec extends WordSpec with GuiceOneAppPerSuite with Matchers with OptionValues {
+
+  private[this] val logger = RollbarProvider.logger("test")
+
+  def createCache[K, V](
+    reloadPeriod: FiniteDuration,
+    retrieve: () => Future[Map[K, V]],
+    maxAttempts: Int = 1
+  ): RefreshingBulkCacheAsync[K, V] =
+    RefreshingBulkCacheAsync.fromActorSystem(retrieve, app.actorSystem, logger, reloadPeriod, maxAttempts)
+
+  "RefreshingBulkCacheAsync" should {
+
+    "retrieve and get" in {
+      val cache = createCache[String, Int](1.minute, () => Future.successful(Map("1" -> 1)))
+
+      cache.get shouldBe Map("1" -> 1)
+      cache.getKey("1").value shouldBe 1
+      cache.getKey("2") shouldBe None
+    }
+
+  }
+
+}

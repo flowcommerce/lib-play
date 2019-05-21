56b93e10cb445caf5d08df581849d5ceb9dd0058
mikeclimbrock <roth@flow.io>
2019-05-21 15:15:14 -0400

    indentation

--- app/io/flow/play/controllers/FlowRequests.scala
+++ app/io/flow/play/controllers/FlowRequests.scala
@@ -5,48 +5,48 @@ import io.flow.play.util._
 import play.api.mvc._
 
 class AnonymousRequest[A](
-                           val auth: AuthData.Anonymous,
-                           request: Request[A]
-                         ) extends WrappedRequest[A](request) {
+  val auth: AuthData.Anonymous,
+  request: Request[A]
+) extends WrappedRequest[A](request) {
   val user: Option[UserReference] = auth.user
 }
 
 class SessionOrgRequest[A](
-                            val auth: OrgAuthData.Session,
-                            request: Request[A]
-                          ) extends WrappedRequest[A](request) {
+  val auth: OrgAuthData.Session,
+  request: Request[A]
+) extends WrappedRequest[A](request) {
   val flowSession: FlowSession = auth.session
   val organization: String = auth.organization
   val environment: Environment = auth.environment
 }
 
 class IdentifiedRequest[A](
-                            val auth: AuthData.Identified,
-                            request: Request[A]
-                          ) extends WrappedRequest[A](request) {
+  val auth: AuthData.Identified,
+  request: Request[A]
+) extends WrappedRequest[A](request) {
   val user: UserReference = auth.user
 }
 
 class SessionRequest[A](
-                         val auth: AuthData.Session,
-                         request: Request[A]
-                       ) extends WrappedRequest[A](request) {
+  val auth: AuthData.Session,
+  request: Request[A]
+) extends WrappedRequest[A](request) {
   val flowSession: FlowSession = auth.session
 }
 
 class IdentifiedOrgRequest[A](
-                               val auth: OrgAuthData.Identified,
-                               request: Request[A]
-                             ) extends WrappedRequest[A](request) {
+  val auth: OrgAuthData.Identified,
+  request: Request[A]
+) extends WrappedRequest[A](request) {
   val user: UserReference = auth.user
   val organization: String = auth.organization
   val environment: Environment = auth.environment
 }
 
 class CustomerRequest[A](
-                                val auth: OrgAuthData.Customer,
-                                request: Request[A]
-                              ) extends WrappedRequest[A](request) {
+  val auth: OrgAuthData.Customer,
+  request: Request[A]
+) extends WrappedRequest[A](request) {
   val organization: String = auth.organization
   val environment: Environment = auth.environment
   val flowSession: FlowSession = auth.session
@@ -57,9 +57,9 @@ class CustomerRequest[A](
   * Any type of request that contains org data
   */
 class OrgRequest[A](
-                     val auth: OrgAuthData,
-                     request: Request[A]
-                   ) extends WrappedRequest[A](request) {
+  val auth: OrgAuthData,
+  request: Request[A]
+) extends WrappedRequest[A](request) {
   val organization: String = auth.organization
   val environment: Environment = auth.environment
 }

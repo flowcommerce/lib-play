04ea0d423b0bed3c0bc3d8a337a6cf47269c6e6f
mikeclimbrock <roth@flow.io>
2019-05-21 14:37:46 -0400

    wip

--- app/io/flow/play/controllers/Authorization.scala
+++ app/io/flow/play/controllers/Authorization.scala
@@ -10,11 +10,6 @@ import scala.util.Try
 
 trait Authorization
 
-case class JwtIdentifiedToken(
-  order: String,
-  session: String,
-  customer: Option[String]
-) extends Authorization
 case class JwtToken(userId: String) extends Authorization
 case class Token(token: String) extends Authorization
 
@@ -34,10 +29,7 @@ class AuthorizationImpl @Inject() (
   /**
     * Parses the actual authorization header value. Acceptable types are:
     * - Basic - the API Token for the user.
-    * - Bearer (one of)
-    *   - the JWT Token for the user that contains an id field representing the user id in the database
-    *   - the JWT Token for the order that contains an order field representing the order number in the database
-    *     with an optional customer field representing the customer number in the database
+    * - Bearer - the JWT Token for the user with that contains an id field representing the user id in the database
     */
   def get(headerValue: String): Option[Authorization] = {
     headerValue.split(" ").toList match {
@@ -53,25 +45,8 @@ class AuthorizationImpl @Inject() (
           case JsonWebToken(_, claimsSet, _) if jwtIsValid(value) => createJwtToken(claimsSet)
           case JsonWebToken(_, claimsSet, _) =>
             val tokenData = createJwtToken(claimsSet)
-
-            val data = tokenData match {
-              case Some(t) =>
-                t match {
-                  case userToken: JwtToken => userToken.userId
-                  case identifiedToken: JwtIdentifiedToken =>
-                    (Seq(identifiedToken.order)
-                      ++ identifiedToken.session
-                      ++ identifiedToken.customer
-                      ).mkString("-")
-                  case _ => "unknown"
-                }
-
-              case _ => "unknown"
-
-            }
-
             logger.
-              withKeyValue("data", data).
+              withKeyValue("user_id", tokenData.map(_.userId).getOrElse("unknown")).
               info("JWT Token was invalid, bad salt")
             None
           case _ => None
@@ -82,25 +57,11 @@ class AuthorizationImpl @Inject() (
   }
 
   private[this] def jwtIsValid(token: String): Boolean =
-    // swallow errors when decoding - for instance algo not supported
+  // swallow errors when decoding - for instance algo not supported
     Try(JsonWebToken.validate(token, jwtSalt)).getOrElse(false)
 
-  private[this] def createJwtToken(claimsSet: JwtClaimsSetJValue): Option[Authorization] =
+  private[this] def createJwtToken(claimsSet: JwtClaimsSetJValue): Option[JwtToken] =
     claimsSet.asSimpleMap.toOption.flatMap { claims =>
-      val userIdToken = claims.get("id").map(JwtToken)
-      val identifiedToken =
-        (claims.get("order"), claims.get("session")) match {
-          case (Some(o), Some(s)) =>
-            Some(
-              JwtIdentifiedToken(
-                order = o,
-                session = s,
-                customer = claims.get("customer")
-              )
-            )
-          case _ => None
-        }
-
-      userIdToken.orElse(identifiedToken)
+      claims.get("id").map(JwtToken)
     }
 }

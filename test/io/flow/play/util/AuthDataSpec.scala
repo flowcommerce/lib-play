3d0e66fdb3c172275032d83d4459413e841ea9f9
Mike Roth <roth@flow.io>
2019-05-27 20:44:16 -0400

    [Returning Customers] Wrapper to auth customer org  or session (#302)

--- test/io/flow/play/util/AuthDataSpec.scala
+++ test/io/flow/play/util/AuthDataSpec.scala
@@ -1,8 +1,12 @@
 package io.flow.play.util
 
 import io.flow.common.v0.models.{Environment, Role, UserReference}
+import io.flow.log.RollbarProvider
+import io.flow.play.util.AuthDataMap.Fields
 
 class AuthDataSpec extends LibPlaySpec {
+
+  private[this] val logger = RollbarProvider.logger("test")
 
   "AuthData.user" in {
     val id = "user-1"
@@ -32,6 +36,39 @@ class AuthDataSpec extends LibPlaySpec {
 
     val all = 1.to(100).map { _ => AuthHeaders.generateRequestId("foo") }
     all.distinct.size must be(100)
+  }
+
+  "OrgAuthData.Checkout - customer" in {
+    val orgAuthDataCustomer = AuthHeaders.organizationCustomer(org = createTestId())
+    val customerData: Map[String, String] = Map(
+      Fields.CreatedAt -> orgAuthDataCustomer.createdAt.toString,
+      Fields.RequestId -> orgAuthDataCustomer.requestId,
+      Fields.Organization -> orgAuthDataCustomer.organization,
+      Fields.Environment -> orgAuthDataCustomer.environment.toString,
+      Fields.Session -> orgAuthDataCustomer.session.id,
+      Fields.Customer -> orgAuthDataCustomer.customer.number
+    )
+
+    val auth: Option[AuthData] = OrgAuthData.Checkout.fromMap(customerData)(logger)
+
+    auth.get.isInstanceOf[OrgAuthData.Customer] must be(true)
+    val customer = auth.get.asInstanceOf[OrgAuthData.Customer]
+    customer must be(orgAuthDataCustomer)
+  }
+
+  "OrgAuthData.Checkout - session" in {
+    val authDataSession = AuthHeaders.session()
+    val sessionData: Map[String, String] = Map(
+      Fields.CreatedAt -> authDataSession.createdAt.toString,
+      Fields.RequestId -> authDataSession.requestId,
+      Fields.Session -> authDataSession.session.id,
+    )
+
+    val auth: Option[AuthData] = OrgAuthData.Checkout.fromMap(sessionData)(logger)
+
+    auth.get.isInstanceOf[AuthData.Session] must be(true)
+    val session = auth.get.asInstanceOf[AuthData.Session]
+    session must be(authDataSession)
   }
 
 }

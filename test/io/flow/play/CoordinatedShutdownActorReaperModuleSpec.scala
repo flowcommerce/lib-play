8d9500b1e8a648e61737ff537029a9bc1be1cdd3
Jack Lynch <jack@flow.io>
2024-03-13 20:28:20 +0000

    FIN-2054 Fix phase used in managed shutdown

--- test/io/flow/play/CoordinatedShutdownActorReaperModuleSpec.scala
+++ test/io/flow/play/CoordinatedShutdownActorReaperModuleSpec.scala
@@ -4,26 +4,18 @@ import akka.pattern.ask
 import akka.util.Timeout
 import io.flow.akka.actor.{ManagedShutdownPhase, ReaperActor}
 import io.flow.play.util.LibPlaySpec
-import org.scalatest.BeforeAndAfterAll
 import org.scalatest.matchers.must.Matchers
-import play.api.Application
-import play.api.inject.guice.GuiceApplicationBuilder
 
 import scala.concurrent.Await
 import scala.concurrent.duration._
 
-class CoordinatedShutdownActorReaperModuleSpec extends LibPlaySpec with BeforeAndAfterAll with Matchers {
-
-  override def fakeApplication(): Application =
-    new GuiceApplicationBuilder()
-      .bindings(new CoordinatedShutdownActorReaperModule())
-      .build()
+class CoordinatedShutdownActorReaperModuleSpec extends LibPlaySpec with Matchers {
 
   "CoordinatedShutdownActorReaperModule" should {
     def verify(phase: ManagedShutdownPhase) = {
-      // Look it up this way to verify that the module registered it (via its extension)
+      // Look it up this way to verify that the module registered it (via its extension loaded in test.conf)
       val reaper = app.actorSystem.actorSelection("/user/" + ReaperActor.name(phase))
-      implicit val timeout: Timeout = 3.seconds
+      implicit val timeout: Timeout = 10.seconds
       Await.result(reaper ? ReaperActor.Reap, timeout.duration) mustBe akka.Done
     }
 

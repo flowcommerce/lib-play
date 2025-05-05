bbb50c97b4c7300e3efd389957ed515d5cea5463
Jean Logeart <jean.logeart@gmail.com>
2025-05-05 15:31:16 -0400

    Add test

--- test/io/flow/play/util/RetrySpec.scala
+++ test/io/flow/play/util/RetrySpec.scala
@@ -4,6 +4,7 @@ import org.mockito.Mockito._
 import org.scalatest.concurrent.PatienceConfiguration.Timeout
 import org.scalatest.concurrent.ScalaFutures
 import org.scalatestplus.mockito.MockitoSugar
+import play.api.libs.concurrent.Futures
 
 import scala.concurrent.duration._
 import scala.concurrent.{ExecutionContext, Future}
@@ -12,6 +13,7 @@ class RetryFuturesSpec extends LibPlaySpec with ScalaFutures with MockitoSugar {
 
   private val retryFutures = app.injector.instanceOf[RetryFutures]
   implicit private val ec: ExecutionContext = app.actorSystem.dispatcher
+  private val futures = app.injector.instanceOf[Futures]
 
   def config(
     delay: FiniteDuration = 10.millis,
@@ -74,6 +76,23 @@ class RetryFuturesSpec extends LibPlaySpec with ScalaFutures with MockitoSugar {
       retryFutures.run(conf)(f.apply()).failed.futureValue(Timeout(1.second)) mustBe a[IllegalArgumentException]
     }
 
+    "retry once when first attempt exceeds timeout" in {
+      val f = mock[() => Future[Int]]
+
+      when(f.apply())
+        .thenReturn(futures.delayed(200.millis)(Future.successful(1))) // simulate slow Future
+        .thenReturn(Future.successful(42)) // Succeeds on retry
+
+      val conf = RetryConfiguration
+        .withFixedDelay(delay = 10.millis, maxAttempts = 2)
+        .withTimeout(Some(50.millis)) // This will trigger a TimeoutException
+
+      val result = retryFutures.run(conf)(f.apply()).futureValue(Timeout(1.second))
+
+      result mustBe 42
+      verify(f, times(2)).apply() // Confirm exactly 2 calls (1 timeout + 1 retry)
+    }
+
     "retry and succeed before overall timeout" in {
       val f = mock[() => Future[Int]]
       when(f.apply())
@@ -110,6 +129,7 @@ class RetryFuturesSpec extends LibPlaySpec with ScalaFutures with MockitoSugar {
       // Verify we called the function 4 times
       verify(f, times(4)).apply()
     }
+
   }
 }
 

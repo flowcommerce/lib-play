53bcf033f1cbe327bc864cc34ec05102997cc5e2
Jack Lynch <jack@flow.io>
2025-07-16 13:21:29 +0100

    FDN-3751 Exclude conflicting module in lib-standalone (#574)

--- standalone/src/main/scala/io/flow/play/standalone/StandaloneApp.scala
+++ standalone/src/main/scala/io/flow/play/standalone/StandaloneApp.scala
@@ -52,8 +52,14 @@ trait StandaloneApp {
     * class MyAppSpec extends PlaySpec with GuiceOneAppPerSuite { override def fakeApplication(): Application = MyApp.build() }
     * }}}
     */
-  def build(): Application = new GuiceApplicationBuilder(environment).build()
+  def build(): Application = {
+    // We provide io.flow.play.standalone.FlowConfigModule, disable this one if present
+    val excludedModules = StandaloneApp.resolveModuleClasses("io.flow.play.clients.ConfigModule")
 
+    new GuiceApplicationBuilder(environment)
+      .disable(excludedModules: _*)
+      .build()
+  }
 }
 
 object StandaloneApp {
@@ -85,5 +91,13 @@ object StandaloneApp {
       case NonFatal(t) =>
         t.printStackTrace()
         throw t
+    }
+
+  def resolveModuleClasses(classNames: String*): Seq[Class[_]] =
+    classNames.flatMap { name =>
+      scala.util.Try(Class.forName(name)).toOption.filter { cls =>
+        classOf[com.google.inject.Module].isAssignableFrom(cls) ||
+        classOf[play.api.inject.Module].isAssignableFrom(cls)
+      }
     }
 }
